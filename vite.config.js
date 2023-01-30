86da7397c677653881b998b0dede10c4a6a30210
Andy Christianson <nix@nixxed.com>
2023-01-30 12:10:08 -0600

    build: Add bundle visualization plugin.

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import { visualizer } from "rollup-plugin-visualizer"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,6 +8,6 @@ export default defineConfig({
     server: {
         port: 3000
     },
-    plugins: [react()],
+    plugins: [react(), visualizer()],
     assetsInclude: ['**/*.md']
 })
