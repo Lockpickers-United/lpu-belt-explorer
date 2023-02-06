7fd68959cfac3f9b5580d3549320cc4ca9da265c
Andy Christianson <nix@nixxed.com>
2023-02-06 13:35:50 -0600

    fix: Prevent duplicate filters on chip click.

--- src/FilterChip.jsx
+++ src/FilterChip.jsx
@@ -3,13 +3,17 @@ import Chip from '@mui/material/Chip'
 import queryString from 'query-string'
 
 function FilterChip({field, value, ...props}) {
-    const handleClick = () => {
+    const handleClick = event => {
+        event.preventDefault()
+
         const query = queryString.parse(location.search)
         const queryValue = query[field]
-        if (Array.isArray(queryValue)) queryValue.push(value)
-        else if (queryValue) query[field] = [queryValue, value]
-        else query[field] = value
-        location.search = queryString.stringify(query)
+        if ((Array.isArray(queryValue) && !queryValue.includes(value)) || queryValue !== value) {
+            if (Array.isArray(queryValue)) queryValue.push(value)
+            else if (queryValue) query[field] = [queryValue, value]
+            else query[field] = value
+            setTimeout(() => location.search = queryString.stringify(query), 100)
+        }
     }
 
     return (
