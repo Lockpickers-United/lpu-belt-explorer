d196d71fdd27e7ae94ede0d3710a91c00d12bc1a
NiXXeD <NiXXeD@users.noreply.github.com>
2023-02-28 17:49:09 +0000

    deploy: 11ee7d2aff1dbaf8bc27e0f9ca6bc8d1d8eea84e

---
+++ assets/beltRequirements-9f8fb9e8.js
@@ -0,0 +1,156 @@
+const e=`This is the beginning of your journey of self-discovery that will test the limits of your mind, body, and spirit. Your belt is white because you are like freshly fallen snow, pure, true of heart and ready to be molded into a lockpicking machine.
+
+Both photos and videos of your picking accomplishment are accepted for this rank.
+
+#### Requirements:
+
+1.  Pick any lock with any tool.`,n=`You understand the basic function of locks, and technique to pick them. Normal padlocks crumble at your very glance and you've moved onto higher quality cylinders, some of which may even contain security pins.
+
+Both photos and videos of your picking accomplishment are accepted for this rank.
+
+#### Requirements:
+
+1.  Pick one of the locks ranked as yellow belt or similar`,t=`Those security pins designed to keep you out are powerless against you. You're starting to find locks containing them easier to pick than those without. Your family is also starting to look at you and your growing collection of locks a bit strangely. It’s OK -- we understand you. Let's get an orange belt on you to confuse them further.
+
+You must submit a **video** of your picking accomplishments for this rank. Videos must be in one, continuous take.  
+Photographs will also be accepted for locks that cannot be disassembled.
+
+#### Requirements:
+
+1.  Pick one of the locks ranked as orange belt or similar`,o=`Higher quality locks with even more security pins. Some of these locks have
+dead cores, and it’s not just because you've picked them to death. It might
+also be that you have started down the dimple lock road. You've also started
+your development as a lock surgeon, gutting them and getting them back
+together again.
+
+You must **video picking and gutting accomplishments in one continuous take**,
+pictures are no longer accepted at this rank, even for locks that cannot be
+disassembled.
+
+- FOR NON GUTTABLE LOCKS (IE. Master 410 LOTO) Video documentation of
+  gutting and reassembling a different lock will be accepted.
+
+Check out the KOD [guide to making pick and gut videos](https://youtu.be/gcekRz-7If8) if you not familiar with the
+process. Remember, [vertical video hurts](https://www.youtube.com/watch?v=dechvhb0Meo).
+
+Requirements:
+
+1. Demonstrate your ability to gut locks.
+2. Demonstrate your ability to reassemble locks (the reassembly can be a different lock and even in a separate video).
+3. SPP (single pin pick) one of the locks ranked as green belt or similar.
+`,i=`Transitionary phase in your mastery. You are leaving the relative calm and
+predictable world of standard locks behind and dipping a toe into the
+strange and bizarre world of high-security locks. You are frequently
+pinning your own locks, making challenge locks, and helping newer members
+of the lockpicking community as they progress.
+
+You must video picking and gutting accomplishments in one continuous take.
+
+#### Requirements:
+
+1. Demonstrate your ability to gut locks and get them back together again
+2. Demonstrated history of helping new pickers, either on the sub and/or
+   in the Discord channel
+3. Have built a "Challenge Lock" and shared it with a fellow picker. Please visit the "Read More..." link below for more
+   information on this requirement.
+4. SPP and gut one of the locks ranked as blue belt or similar
+
+Note: If picking an SFIC, be sure to review the guidelines listed under SFIC Filming Requirements for validating
+your lock in the picking video. Please visit the "Read More..." link below for filming requirements.`,a=`You are now picking locks that are categorically hi-sec. They have two or
+more discrete locking mechanisms and are considered unpickable by nearly
+every locksmith on the planet. You are helping new pickers frequently
+and sharing challenge locks.
+
+You must video picking and gutting accomplishments in one continuous take.
+
+#### Requirements:
+
+1. Demonstrate your ability to gut locks and get them back together again
+2. Demonstrated history of helping new pickers
+3. Have built a "Challenge Lock" and shared it with a fellow picker. Please visit the "Read More..." link below for more
+   information.
+4. SPP and gut two different locks ranked as Purple Belt (locks have to be from separate entries in the list) or
+   similar.
+
+If picking an SFIC, be sure to review the guidelines listed under SFIC Filming Requirements for validating your lock in
+the picking video. Please visit the "Read More..." link below for more filming requirements.
+`,s=`You are entering the realm of ultra high-security locks and becoming a
+pillar of the lockpicking community. You find yourself picking locks that
+are so difficult tools sometimes don’t even exist to open them, requiring
+you make your own. Tight keyways, rotating sidebar fingers, telescoping
+keypins, and countermilling are the norm for you.
+
+You must video picking and gutting accomplishments in one continuous take.
+
+#### Requirements:
+
+1. Demonstrate your ability to gut locks and get them back together again
+2. Demonstrated history of helping advancing and advanced pickers
+3. Have built a "Challenge Lock" and shared it with a fellow picker. Please visit the "Read More..." link below for more
+   information.
+4. Have made at least one specialized tool
+5. SPP and gut two different locks ranked as Brown Belt (locks have to be from separate entries on the list) or similar
+`,r=`Ultra high security is your middle name. Your lock collection is worth more than
+your first car and you constantly troll eBay for deals on insane European locks.
+You are frequently making custom tools to pick difficult locks, and you are acting
+as a mentor to folks at the blue, purple and brown belt levels.
+
+You must video picking and gutting accomplishments in one continuous take.
+
+Requirements:
+
+1. Demonstrate your ability to gut locks and get them back together again
+2. Demonstrated history of helping advancing and advanced pickers
+3. Have built a "Challenge Lock" and shared it with a fellow picker. Please visit the "Read More..." link below for more
+   information.
+4. Have made at least one specialized tool
+5. Mentor a more advanced picker through the process of getting into a difficult lock
+6. SPP and gut two different locks ranked as Red Belt (locks have to be from separate entries on the list) or similar
+`,l=`Zen music plays when you enter the room. You wake up from dreams in which you are trying to figure out ways to pick the
+unpickable. You have mentored two or more pickers at the brown and red belt levels and you have published the specs to
+at least one custom tool of your own design.
+
+To earn a black belt, world-class picking is necessary but not sufficient. Mentoring others is necessary but not
+sufficient. You must deviate from your comfort zone and complete at least two epic quests of sufficient complexity and
+difficulty. Black Belt Pickers are extremely rare, and while this system was designed with a degree of levity, earning
+one is true honor and something to be very very proud of.
+
+You must video picking and gutting accomplishments in one continuous take, If there is a sidebar it must also be
+displayed to show nothing has been tampered with. There are no exceptions to the gutting requirement for videos of black
+belt locks.
+
+In gutting, all components relevant to function must be shown clearly and zoomed in. This varies by lock and it is
+recommended applicants ask in the discord or via modmail prior to filming. All locks must be gutted, even if destruction
+is required to gut.
+
+Requirements:
+
+1. Demonstrate your ability to gut locks and get them back together again
+2. Demonstrated history of helping advancing and advanced pickers
+3. Have built a "Challenge Lock" and shared it with a fellow picker. Please visit the "Read More..." link below for more
+   information.
+4. Have made at least one specialized tool and published the specs
+5. Mentor at least two advanced pickers through the process of getting into a difficult lock
+6. Complete at least two of the "Epic Quest Options" listed below
+7. SPP and gut two different locks ranked as Black Belt (locks have to be from separate entries on the list) or similar
+
+Epic Quest Options (all subject to approval):
+
+- Develop and demonstrate an "Extra Skill" that is related to picking or physical security
+- Complete and document a "Master's Project" of sufficient difficulty and complexity
+- Select and pick two additional Black Belt level locks from the list
+
+Examples of "Extra Skills":
+
+- Group 2 safe lock manipulation
+- Electronics projects that decode or bypass locks
+- Key impressioning
+- Advanced and refined Pick making
+- Machining cutaway locks
+- Exceptional knowledge of locks and keyways
+
+Examples of "Master's Projects":
+
+- Make a lock from scratch
+- Formation and leadership of a TOOOL chapter or other community organization
+`,c={white:e,yellow:n,orange:t,green:o,blue:i,purple:a,brown:s,red:r,black:l};export{c as default};
