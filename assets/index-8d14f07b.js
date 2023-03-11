100f20af48b07db4c0950c1c8b17e91e3713cb99
mgsecure <mgsecure@users.noreply.github.com>
2023-03-11 20:05:51 +0000

    deploy: f9b452e3337001913a6410e2b54c0d2712078428

--- assets/index-8d14f07b.js
+++ assets/index-8d14f07b.js
@@ -157,7 +157,58 @@ As a way to have fun (perhaps at the expense of Six Sigma) this sub gives its me
 Karate Belt Flair is entirely optional and members will go "unranked" until they specifically requests ranking by [messaging the moderation team](https://www.reddit.com/message/compose?to=%2Fr%2Flockpicking). The moderators do not actively monitor the sub and all requests must go through modmail, posts requesting a belt will be removed.
 
 Remember this is all just for fun... In the end, no matter what karate belt you wear, you're still just wearing fancy pajamas.
-`,bB=`## FAQ
+`,bB=`## Belt Request Instructions
+
+### Reddit
+
+Belts should always be requested via modmail on Reddit. Posts requesting belts will be excluded.
+If you want to sync the new belt with your user on Discord, please add your Discord username with the request.
+
+Each modmail should include:
+
+- A link to the photo / video of the lock(s) picked.
+- The model of the lock that was picked.
+- Specify what belt you are looking to rank up to.
+- If the video is long, include timestamps for major events in the video (Lock opened @ X time) if timestamps are in the description that is fine.
+- FOR PHOTOS - Ensure keyway is visible and clear in a picked state. We are not concerned with seeing the shackle open.
+- FOR VIDEOS - Ensure clarity and proper light showing all parts of the lock during gutting process (The lock can be
+      fully gutted and a zoom in after to show parts is acceptable). Please avoid using the Shorts format on YouTube, it
+      does not allow for rewinding to see anything that was missed without watching the whole video over.
+
+Under upper tier ranks that have multiple requirements please collect all the requirements in to one modmail and make
+one request, this makes it easier to go through that modmail and ensure all requirements are filled out rather than
+going back through old archived modmail to find the remainder of your requirements.
+
+**Any request that is missing any component will be rejected, Any request that is repeated will be put to the back of
+the line as we move oldest requests out first.**
+
+**Requests may take up to 5 days to process**
+
+**HOW TO SEND A MODMAIL**
+
+Click the "Message the moderators" Link on the sidebar just above the list of moderators for this subreddit, Or
+use  [THIS LINK.](https://www.reddit.com/message/compose?to=%2Fr%2Flockpicking)
+
+### Discord
+
+All requests are made in #belt-requests throught the belt bot using the following structure:
+
+\`\`\`
+@LPUBeltBot request BELTCOLOR https://image_or_video_link_here
+\`\`\`
+
+If you wish to sync the belt to your user on Reddit you should use the following structure:
+
+\`\`\`
+@LPUBeltBot request BELTCOLOR sync to /u/redditusername https://image_or_video_link_here
+\`\`\`
+
+Any additional information can be added below in different lines if necessary, but the above formula must be in one
+line.
+
+----------
+
+## FAQ
 
 Q: **Must I begin at "White Belt Picker" and progress through the ranks, or can I skip directly to the belt that
 is appropriate for me?**
@@ -319,59 +370,6 @@ the internet. Check it out [here](https://docs.google.com/document/d/1w33AkXvtXb
     - It is recommended that you start by removing any master wafers which are not required for the key to function. If
       you cannot remove sufficient wafers this way, then you may be required to pick and gut the lock without a working
       key.
-
-----------
-
-## Belt Request Instructions
-
-### Reddit
-
-Belts should always be requested via modmail on Reddit. Posts requesting belts will be excluded.
-If you want to sync the new belt with your user on Discord, please add your Discord username with the request.
-
-Each modmail should include:
-
-- A link to the photo / video of the lock(s) picked.
-- The model of the lock that was picked.
-- Specify what belt you are looking to rank up to.
-- If the video is long, include timestamps for major events in the video (Lock opened @ X time) if timestamps are in the description that is fine.
-- FOR PHOTOS - Ensure keyway is visible and clear in a picked state. We are not concerned with seeing the shackle open.
-- FOR VIDEOS - Ensure clarity and proper light showing all parts of the lock during gutting process (The lock can be
-      fully gutted and a zoom in after to show parts is acceptable). Please avoid using the Shorts format on YouTube, it
-      does not allow for rewinding to see anything that was missed without watching the whole video over.
-
-Under upper tier ranks that have multiple requirements please collect all the requirements in to one modmail and make
-one request, this makes it easier to go through that modmail and ensure all requirements are filled out rather than
-going back through old archived modmail to find the remainder of your requirements.
-
-**Any request that is missing any component will be rejected, Any request that is repeated will be put to the back of
-the line as we move oldest requests out first.**
-
-**Requests may take up to 5 days to process**
-
-**HOW TO SEND A MODMAIL**
-
-Click the "Message the moderators" Link on the sidebar just above the list of moderators for this subreddit, Or
-use  [THIS LINK.](https://www.reddit.com/message/compose?to=%2Fr%2Flockpicking)
-
-----------
-
-### Discord
-
-All requests are made in #belt-requests throught the belt bot using the following structure:
-
-\`\`\`
-@LPUBeltBot request BELTCOLOR https://image_or_video_link_here
-\`\`\`
-
-If you wish to sync the belt to your user on Reddit you should use the following structure:
-
-\`\`\`
-@LPUBeltBot request BELTCOLOR sync to /u/redditusername https://image_or_video_link_here
-\`\`\`
-
-Any additional information can be added below in different lines if necessary, but the above formula must be in one
-line.
 
 ----------
 
--- belts.md
+++ belts.md
@@ -975,6 +975,57 @@ Examples of "Master's Projects":
 
 ---
 
+## Belt Request Instructions
+
+### Reddit
+
+Belts should always be requested via modmail on Reddit. Posts requesting belts will be excluded.
+If you want to sync the new belt with your user on Discord, please add your Discord username with the request.
+
+Each modmail should include:
+
+- A link to the photo / video of the lock(s) picked.
+- The model of the lock that was picked.
+- Specify what belt you are looking to rank up to.
+- If the video is long, include timestamps for major events in the video (Lock opened @ X time) if timestamps are in the description that is fine.
+- FOR PHOTOS - Ensure keyway is visible and clear in a picked state. We are not concerned with seeing the shackle open.
+- FOR VIDEOS - Ensure clarity and proper light showing all parts of the lock during gutting process (The lock can be
+      fully gutted and a zoom in after to show parts is acceptable). Please avoid using the Shorts format on YouTube, it
+      does not allow for rewinding to see anything that was missed without watching the whole video over.
+
+Under upper tier ranks that have multiple requirements please collect all the requirements in to one modmail and make
+one request, this makes it easier to go through that modmail and ensure all requirements are filled out rather than
+going back through old archived modmail to find the remainder of your requirements.
+
+**Any request that is missing any component will be rejected, Any request that is repeated will be put to the back of
+the line as we move oldest requests out first.**
+
+**Requests may take up to 5 days to process**
+
+**HOW TO SEND A MODMAIL**
+
+Click the "Message the moderators" Link on the sidebar just above the list of moderators for this subreddit, Or
+use  [THIS LINK.](https://www.reddit.com/message/compose?to=%2Fr%2Flockpicking)
+
+### Discord
+
+All requests are made in #belt-requests throught the belt bot using the following structure:
+
+```
+@LPUBeltBot request BELTCOLOR https://image_or_video_link_here
+```
+
+If you wish to sync the belt to your user on Reddit you should use the following structure:
+
+```
+@LPUBeltBot request BELTCOLOR sync to /u/redditusername https://image_or_video_link_here
+```
+
+Any additional information can be added below in different lines if necessary, but the above formula must be in one
+line.
+
+----------
+
 ## FAQ
 
 Q: **Must I begin at "White Belt Picker" and progress through the ranks, or can I skip directly to the belt that
@@ -1137,59 +1188,6 @@ the internet. Check it out [here](https://docs.google.com/document/d/1w33AkXvtXb
     - It is recommended that you start by removing any master wafers which are not required for the key to function. If
       you cannot remove sufficient wafers this way, then you may be required to pick and gut the lock without a working
       key.
-
-----------
-
-## Belt Request Instructions
-
-### Reddit
-
-Belts should always be requested via modmail on Reddit. Posts requesting belts will be excluded.
-If you want to sync the new belt with your user on Discord, please add your Discord username with the request.
-
-Each modmail should include:
-
-- A link to the photo / video of the lock(s) picked.
-- The model of the lock that was picked.
-- Specify what belt you are looking to rank up to.
-- If the video is long, include timestamps for major events in the video (Lock opened @ X time) if timestamps are in the description that is fine.
-- FOR PHOTOS - Ensure keyway is visible and clear in a picked state. We are not concerned with seeing the shackle open.
-- FOR VIDEOS - Ensure clarity and proper light showing all parts of the lock during gutting process (The lock can be
-      fully gutted and a zoom in after to show parts is acceptable). Please avoid using the Shorts format on YouTube, it
-      does not allow for rewinding to see anything that was missed without watching the whole video over.
-
-Under upper tier ranks that have multiple requirements please collect all the requirements in to one modmail and make
-one request, this makes it easier to go through that modmail and ensure all requirements are filled out rather than
-going back through old archived modmail to find the remainder of your requirements.
-
-**Any request that is missing any component will be rejected, Any request that is repeated will be put to the back of
-the line as we move oldest requests out first.**
-
-**Requests may take up to 5 days to process**
-
-**HOW TO SEND A MODMAIL**
-
-Click the "Message the moderators" Link on the sidebar just above the list of moderators for this subreddit, Or
-use  [THIS LINK.](https://www.reddit.com/message/compose?to=%2Fr%2Flockpicking)
-
-----------
-
-### Discord
-
-All requests are made in #belt-requests throught the belt bot using the following structure:
-
-```
-@LPUBeltBot request BELTCOLOR https://image_or_video_link_here
-```
-
-If you wish to sync the belt to your user on Reddit you should use the following structure:
-
-```
-@LPUBeltBot request BELTCOLOR sync to /u/redditusername https://image_or_video_link_here
-```
-
-Any additional information can be added below in different lines if necessary, but the above formula must be in one
-line.
 
 ----------
 
