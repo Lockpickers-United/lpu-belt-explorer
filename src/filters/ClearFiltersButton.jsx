35fbf947145f278d62819a5255e999755c0fd78b
Andy Christianson <nix@nixxed.com>
2023-03-16 16:21:54 -0500

    fix: Clear Filters button should be consistent width.

--- src/filters/ClearFiltersButton.jsx
+++ src/filters/ClearFiltersButton.jsx
@@ -2,18 +2,19 @@ import React, {useContext} from 'react'
 import Button from '@mui/material/Button'
 import FilterContext from '../contexts/FilterContext.jsx'
 import AppContext from '../contexts/AppContext.jsx'
+import {useCallback} from 'react'
 
 function ClearFiltersButton(props) {
     const {tab, setTab} = useContext(AppContext)
     const {filters, clearFilters} = useContext(FilterContext)
 
-    const handleClear = () => {
+    const handleClear = useCallback(() => {
         if (!filters.search && tab === 'search') setTab('White')
         setTimeout(() => clearFilters(), 100)
-    }
+    }, [clearFilters, filters.search, setTab, tab])
 
     return (
-        <Button variant='outlined' color='inherit' onClick={handleClear} {...props}>
+        <Button variant='outlined' color='inherit' onClick={handleClear} style={{minWidth: 120}} {...props}>
             Clear&nbsp;Filters
         </Button>
     )
