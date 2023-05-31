85f9838a471f74a59ef68ac528faa12fe1b42cca
Andy Christianson <nix@nixxed.com>
2023-05-31 12:38:25 -0500

    fix: Clear filters on random click.

--- src/entries/RandomEntryButton.jsx
+++ src/entries/RandomEntryButton.jsx
@@ -3,18 +3,21 @@ import IconButton from '@mui/material/IconButton'
 import Tooltip from '@mui/material/Tooltip'
 import React, {useCallback, useContext} from 'react'
 import AppContext from '../contexts/AppContext'
+import FilterContext from '../contexts/FilterContext'
 import LazyDataContext from '../contexts/LazyDataContext'
 
 function RandomEntryButton() {
     const {data} = useContext(LazyDataContext)
     const {setTab, setExpanded} = useContext(AppContext)
+    const {clearFilters} = useContext(FilterContext)
 
     const handleClick = useCallback(() => {
         const index = Math.floor(Math.random() * data.length)
         const entry = data[index]
+        clearFilters()
         setTab(entry.belt.replace(/\s\d/g, ''))
         setExpanded(entry.id)
-    }, [setTab, setExpanded, data])
+    }, [setTab, setExpanded, clearFilters, data])
 
     return (
         <Tooltip title='Random Lock' arrow disableFocusListener>
