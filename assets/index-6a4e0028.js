8cb1a74fbece6ff2481f244d3401508de00af2ee
mgsecure <mgsecure@users.noreply.github.com>
2023-07-13 20:59:34 +0000

    deploy: d6807dfec14658038d4f50b621d0b96def7237b2

--- assets/index-6a4e0028.js
+++ assets/index-6a4e0028.js
@@ -559,10 +559,10 @@ Visit the #belt-changelog channel in the [Discord](https://discord.gg/lockpickin
 Both photos and videos of your picking accomplishment are accepted for this rank.
 
 **Requirements**
-* Pick any lock with any tool.
+- Pick any lock with any tool.
 
 **Evidence format**
-* Photos or vídeos, showing turned core
+- Photos or vídeos, showing turned core
 
 **White Belt Locks:**
 `,uD=`You understand the basic function of locks, and technique to pick them. Normal padlocks crumble at your very glance and you've moved onto higher quality cylinders, some of which may even contain security pins.
