c4a3f90b3d5b1e27de957632a9db4f10d73695ae
Andy Christianson <nix@nixxed.com>
2023-07-24 15:53:16 -0500

    revert: Image viewer features didn't minify right

--- src/entries/ImageGallery.jsx
+++ src/entries/ImageGallery.jsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useState} from 'react'
+import React, {useCallback} from 'react'
 import ImageList from '@mui/material/ImageList'
 import ImageListItem from '@mui/material/ImageListItem'
 import ImageListItemBar from '@mui/material/ImageListItemBar'
@@ -8,88 +8,66 @@ import IconButton from '@mui/material/IconButton'
 import LaunchIcon from '@mui/icons-material/Launch'
 import useWindowSize from '../util/useWindowSize'
 import ytIcon from '../resources/yt.png'
-import Lightbox from 'react-awesome-lightbox/src/index'
-import 'react-awesome-lightbox/build/style.css'
 
 function ImageGallery({entry}) {
     const {width} = useWindowSize()
     const isMobile = width < 736
-    const [openImage, setOpenImage] = useState(-1)
 
-    const handleVideoClick = useCallback(url => {
+    const handleClick = useCallback(url => {
         return () => window.open(url, '_blank', 'noopener,noreferrer')
     }, [])
 
-    const handleOpen = useCallback(index => () => setOpenImage(index), [])
-    const handleClose = useCallback(() => setOpenImage(-1), [])
-
     return (
-        <React.Fragment>
-            {
-                openImage >= 0 &&
-                <Lightbox
-                    startIndex={openImage}
-                    allowRotate={false}
-                    allowReset={false}
-                    images={entry.media.map(entry => ({
-                        url: entry.fullSizeUrl || entry.thumbnailUrl,
-                        title: `${entry.title} (${entry.subtitle})`
-                    }))}
-                    onClose={handleClose}
-                />
-            }
-            <ImageList variant="masonry" cols={isMobile ? 2 : 3} sx={{marginTop: 0}}>
-                {entry.media.map(({title, subtitle, thumbnailUrl, fullUrl, subtitleUrl}, index) =>
-                    <ImageListItem key={index} style={{marginBottom: 8}}>
+        <ImageList variant='masonry' cols={isMobile ? 2 : 3} sx={{marginTop: 0}}>
+            {entry.media.map(({title, subtitle, thumbnailUrl, fullUrl, subtitleUrl}, index) =>
+                <ImageListItem key={index} style={{marginBottom: 8}}>
+                    <img
+                        src={thumbnailUrl}
+                        alt={title}
+                        style={{paddingBottom: subtitle ? 60 : 48, cursor: 'pointer'}}
+                        onClick={handleClick(fullUrl)}
+                    />
+                    {
+                        fullUrl?.match(/youtube\.com/) &&
                         <img
-                            src={thumbnailUrl}
-                            alt={title}
-                            style={{paddingBottom: subtitle ? 60 : 48, cursor: 'pointer'}}
-                            onClick={handleOpen(index)}
+                            src={ytIcon}
+                            style={{
+                                alignItems: 'center',
+                                position: 'absolute',
+                                top: 'calc(50% - 65px)',
+                                left: 'calc(50% - 40px)',
+                                width: 80,
+                                height: 80,
+                                cursor: 'pointer'
+                            }}
+                            onClick={handleClick(fullUrl)}
                         />
-                        {
-                            fullUrl?.match(/youtube\.com/) &&
-                            <img
-                                src={ytIcon}
-                                alt={title}
-                                style={{
-                                    alignItems: 'center',
-                                    position: 'absolute',
-                                    top: 'calc(50% - 65px)',
-                                    left: 'calc(50% - 40px)',
-                                    width: 80,
-                                    height: 80,
-                                    cursor: 'pointer'
-                                }}
-                                onClick={handleVideoClick(fullUrl)}
-                            />
+                    }
+                    <ImageListItemBar
+                        title={title}
+                        subtitle={
+                            subtitle &&
+                            <a href={subtitleUrl || licenses[subtitle]} target='_blank' rel='noopener noreferrer'>
+                                {subtitle}
+                            </a>
                         }
-                        <ImageListItemBar
-                            title={title}
-                            subtitle={
-                                subtitle &&
-                                <a href={subtitleUrl || licenses[subtitle]} target="_blank" rel="noopener noreferrer">
-                                    {subtitle}
-                                </a>
-                            }
-                            actionIcon={
-                                fullUrl &&
-                                <Tooltip title="View Full Size" arrow disableFocusListener>
-                                    <IconButton
-                                        href={fullUrl}
-                                        style={{color: 'rgba(255, 255, 255, 0.5)'}}
-                                        target="_blank"
-                                        rel="noopener noreferrer"
-                                    >
-                                        <LaunchIcon/>
-                                    </IconButton>
-                                </Tooltip>
-                            }
-                        />
-                    </ImageListItem>
-                )}
-            </ImageList>
-        </React.Fragment>
+                        actionIcon={
+                            fullUrl &&
+                            <Tooltip title='View Full Size' arrow disableFocusListener>
+                                <IconButton
+                                    href={fullUrl}
+                                    style={{color: 'rgba(255, 255, 255, 0.5)'}}
+                                    target='_blank'
+                                    rel='noopener noreferrer'
+                                >
+                                    <LaunchIcon/>
+                                </IconButton>
+                            </Tooltip>
+                        }
+                    />
+                </ImageListItem>
+            )}
+        </ImageList>
     )
 }
 
