4335c5d04f8132d8e7274cbb6ef6b65865c95355
Andy Christianson <nix@nixxed.com>
2023-09-13 10:37:00 -0500

    feat: Improvements to SEO export.

--- scripts/exportHtml.js
+++ scripts/exportHtml.js
@@ -3,6 +3,7 @@ import fs from 'fs'
 
 const rawData = JSON.parse(fs.readFileSync('./src/data/data.json', 'utf8'))
 const template = fs.readFileSync('./src/resources/seo-template.html', 'utf8')
+const mediaTemplate = fs.readFileSync('./src/resources/seo-media-template.html', 'utf8')
 
 if (!fs.existsSync('./dist/locks')) {
     fs.mkdirSync('./dist/locks')
@@ -10,16 +11,25 @@ if (!fs.existsSync('./dist/locks')) {
 
 rawData.forEach(entry => {
     const values = {
+        id: entry.id,
         title: entry.makeModels.map(({make, model}) => {
             return encodeNonAsciiHTML(make && make !== model ? `${make} ${model}` : model)
         }).join (' / '),
         belt: entry.belt,
-        description: entry.version,
+        version: entry.version || '',
+        notes: entry.notes || '',
         image_url: entry.media?.[0]?.thumbnailUrl || '',
-        images: entry.media?.map(image => {
-            return `<img src='${image.thumbnailUrl}' alt='${image.title}'>`
+        images: entry.media?.map(media => {
+            const mediaTokens = Object.keys(media)
+            let mediaOutput = mediaTemplate
+            mediaTokens.forEach(token => {
+                const re = new RegExp(`\\$\\$${token.toUpperCase()}\\$\\$`, 'g')
+                mediaOutput = mediaOutput.replace(re, media[token])
+            })
+            return mediaOutput
         }).join('\n') || '',
-        keywords: (entry.features || []).concat(entry.lockingMechanisms || []).join(', ')
+        features: (entry.features || []).join(', '),
+        locking_mechanisms: (entry.lockingMechanisms || []).join(', ')
     }
     const tokens = Object.keys(values)
     let output = template
@@ -27,6 +37,13 @@ rawData.forEach(entry => {
         const re = new RegExp(`\\$\\$${token.toUpperCase()}\\$\\$`, 'g')
         output = output.replace(re, values[token])
     })
+
+
+    const missed = output.match(/\$\$\w+\$\$/g)
+    if (missed) {
+        console.log('Missed tokens', missed)
+        process.exit(1)
+    }
 
     const filename = `./dist/locks/${entry.id}.html`
     fs.writeFileSync(filename, output)
