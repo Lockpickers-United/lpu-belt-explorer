6a6ea545581afdeef4505fbb9e6d23db750ae122
Andy Christianson <nix@nixxed.com>
2023-09-14 08:13:17 -0500

    fix: Oops.

--- scripts/exportHtml.js
+++ scripts/exportHtml.js
@@ -6,8 +6,24 @@ const template = fs.readFileSync('./src/resources/seo-template.html', 'utf8')
 const mediaTemplate = fs.readFileSync('./src/resources/seo-media-template.html', 'utf8')
 const shareTemplate = fs.readFileSync('./src/resources/seo-share-template.html', 'utf8')
 
-if (!fs.existsSync('./dist/locks')) {
-    fs.mkdirSync('./dist/locks')
+if (!fs.existsSync('./dist/locks')) fs.mkdirSync('./dist/locks')
+if (!fs.existsSync('./dist/share')) fs.mkdirSync('./dist/share')
+
+const replaceValues = (template, values) => {
+    const tokens = Object.keys(values)
+    let output = template
+    tokens.forEach(token => {
+        const re = new RegExp(`\\$\\$${token.toUpperCase()}\\$\\$`, 'g')
+        output = output.replace(re, values[token])
+    })
+
+    const missed = output.match(/\$\$\w+\$\$/g)
+    if (missed) {
+        console.log('Missed tokens', missed)
+        process.exit(1)
+    }
+
+    return output
 }
 
 rawData.forEach(entry => {
@@ -32,20 +48,3 @@ rawData.forEach(entry => {
     fs.writeFileSync(`./dist/locks/${entry.id}.html`, output)
     fs.writeFileSync(`./dist/share/${entry.id}.html`, shareOutput)
 })
-
-const replaceValues = (template, values) => {
-    const tokens = Object.keys(values)
-    let output = template
-    tokens.forEach(token => {
-        const re = new RegExp(`\\$\\$${token.toUpperCase()}\\$\\$`, 'g')
-        output = output.replace(re, values[token])
-    })
-
-    const missed = output.match(/\$\$\w+\$\$/g)
-    if (missed) {
-        console.log('Missed tokens', missed)
-        process.exit(1)
-    }
-
-    return output
-}
