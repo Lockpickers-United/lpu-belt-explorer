49bc835c44eeef4ccfbb24c686ba5a2560a86f80
Andy Christianson <nix@nixxed.com>
2023-11-09 09:56:25 -0600

    feat: Add referrer for analytics.

--- src/app/Tracker.jsx
+++ src/app/Tracker.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import querystring from 'query-string'
 
 function Tracker({id}) {
     if (import.meta.env.DEV) return null
     const randomStuff = (Math.random()).toString(36).substring(2, 10)
     const file = id ? 'clear.gif' : 'welcome.gif'
-    const suffix = id ? `id=${id}&r=${randomStuff}` : `r=${randomStuff}`
-    const url = `https://images.lpubelts.com/i/${file}?${suffix}`
-    return <img src={url} width={0} height={0}/>
+    const ref = document.referrer || 'none'
+    const query = querystring.stringify({id, r: randomStuff, ref})
+    const url = `https://images.lpubelts.com/i/${file}?${query}`
+    return <img alt='' src={url} width={0} height={0}/>
 }
 
 export default React.memo(Tracker)
