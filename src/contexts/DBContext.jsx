610b18f0c4d5f216d2f2ee38dc4e257c5b776538
Andy Christianson <nix@nixxed.com>
2023-12-03 11:25:27 -0600

    fix: Attempt to handle DB error state simply for now.

--- src/contexts/DBContext.jsx
+++ src/contexts/DBContext.jsx
@@ -8,18 +8,21 @@ const DBContext = React.createContext({})
 export function DBProvider({children}) {
     const {isLoggedIn, user} = useContext(AuthContext)
     const [lockCollection, setLockCollection] = useState({})
+    const [dbError, setDbError] = useState(null)
 
     const addToLockCollection = useCallback(async (key, entryId) => {
+        if (dbError) return false
         await updateDoc(doc(db, 'lockcollections', user.uid), {
             [key]: arrayUnion(entryId)
         })
-    }, [user])
+    }, [dbError, user])
 
     const removeFromLockCollection = useCallback(async (key, entryId) => {
+        if (dbError) return false
         await updateDoc(doc(db, 'lockcollections', user.uid), {
             [key]: arrayRemove(entryId)
         })
-    }, [user])
+    }, [dbError, user])
 
     // Lock Collection Subscription
     useEffect(() => {
@@ -28,10 +31,15 @@ export function DBProvider({children}) {
             return onSnapshot(ref, doc => {
                 const data = doc.data()
                 if (data) {
+                    console.info('DB Subscription set up successfully.')
                     setLockCollection(data)
                 } else {
+                    console.info('New user detected, creating initial DB record.')
                     return setDoc(ref, {})
                 }
+            }, error => {
+                console.error('Error listening to DB:', error)
+                setDbError(true)
             })
         } else {
             setLockCollection({})
