85f527b8db59100afd1ba5ed35b7baaa905fe540
Andy Christianson <nix@nixxed.com>
2023-12-03 15:29:20 -0600

    feat: Add script to restore user data.

---
+++ scripts/userCollectionFix.cjs
@@ -0,0 +1,16 @@
+const admin = require('firebase-admin')
+const serviceAccount = require('../../lpu-belt-explorer-firebase-adminsdk.json')
+const app = admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://lpu-belt-explorer.firebaseio.com'
+})
+const db = admin.firestore(app)
+
+// Update these variables to restore a user's data
+const userId = ''
+const data = {
+
+}
+
+const ref = db.doc(`/lockcollections/${userId}`)
+ref.set(data)
