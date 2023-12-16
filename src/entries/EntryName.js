e542afe04ea13eea2b21fb671a80c45c7e6b1e61
Andy Christianson <nix@nixxed.com>
2023-12-16 13:06:49 -0600

    refactor: Clean up code style of EntryName

--- src/entries/EntryName.js
+++ src/entries/EntryName.js
@@ -1,29 +1,31 @@
 /*
-Usage:
-EntryName(entry)			ASSA Twin Combi, Triton, Neptun 4900 / TrioVing System 10, Twin Control
-EntryName(entry,'any', 1)	ASSA Twin Combi, Triton, Neptun 4900 / TrioVing System 10, Twin Control (6 pin with 5 finger pins)
-EntryName(entry,'long')	    ASSA Twin Combi / ASSA Triton / ASSA Neptun 4900 / TrioVing System 10 / TrioVing Twin Control
-EntryName(entry,'data')		ASSA,ASSA,ASSA,TrioVing,TrioVing	Twin Combi,Triton,Neptun 4900,System 10,Twin Control
-EntryName(entry,'array')	['ASSA,ASSA,ASSA,TrioVing,TrioVing', 'Twin Combi,Triton,Neptun 4900,System 10,Twin Control']
+    Usage:
+    EntryName(entry,'any', 1) -> ASSA Twin Combi, Triton, Neptun 4900 / TrioVing System 10, Twin Control (6 pin with 5 finger pins)
+    EntryName(entry, 'short') -> ASSA Twin Combi, Triton, Neptun 4900 / TrioVing System 10, Twin Control
+    EntryName(entry, 'long')  -> ASSA Twin Combi / ASSA Triton / ASSA Neptun 4900 / TrioVing System 10 / TrioVing Twin Control
+    EntryName(entry, 'data')  -> ASSA,ASSA,ASSA,TrioVing,TrioVing	Twin Combi,Triton,Neptun 4900,System 10,Twin Control
+    EntryName(entry, 'array') -> ['ASSA,ASSA,ASSA,TrioVing,TrioVing', 'Twin Combi,Triton,Neptun 4900,System 10,Twin Control']
 */
 
 function EntryName(entry, nameType = 'short', includeVersion = false) {
     const versionString = includeVersion && entry.version ? ' (' + entry.version + ')' : ''
 
     if (nameType === 'long') {
-        let lockName = ''
-        entry.makeModels.forEach((makeModel) => {
-            let lockSep = lockName === '' ? '' : ' / '
-            lockName += lockSep + makeModel.make + ' ' + makeModel.model
-        })
+        const lockName = entry.makeModels.map((makeModel) => {
+            return makeModel.make + ' ' + makeModel.model
+        }).join(' / ')
         return lockName + versionString
     } else if (nameType === 'data') {
-        return entry.makeModels.map(e => e.make).join(',') + '\t'
-            + entry.makeModels.map(e => e.model).join(',')
-            + versionString
+        const makes = entry.makeModels.map(e => e.make).join(',')
+        const models = entry.makeModels.map(e => e.model).join(',')
+        return `${makes}\t${models}` + versionString
     } else if (nameType === 'array') {
-        return [entry.makeModels.map(e => e.make).join(','), entry.makeModels.map(e => e.model).join(',')]
+        return [
+            entry.makeModels.map(e => e.make).join(','),
+            entry.makeModels.map(e => e.model).join(',')
+        ]
     } else {
+        // TODO: Clean up to be a bit more functional style
         let lockName = ''
         let prevMake = ''
         entry.makeModels.forEach((makeModel) => {
