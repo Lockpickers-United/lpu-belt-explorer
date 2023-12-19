f38a0c408e8430e8c34cab6cef28b440e40b5307
Andy Christianson <nix@nixxed.com>
2023-12-19 10:31:35 -0600

    fix: Clean up styles in Leaderboard.

--- src/leaderboard/Leaderboard.jsx
+++ src/leaderboard/Leaderboard.jsx
@@ -1,4 +1,5 @@
 import Button from '@mui/material/Button'
+import Typography from '@mui/material/Typography'
 import {enqueueSnackbar} from 'notistack'
 import React, {useContext, useEffect, useState} from 'react'
 import AuthContext from '../contexts/AuthContext'
@@ -34,36 +35,26 @@ function Leaderboard() {
     const updateTime = dayjs(data.metadata.updatedDateTime).format('MM/DD/YY hh:mm')
 
     return (
-        <React.Fragment>
-            <div style={{
-                padding: '8px', fontSize: '0.8rem', textAlign: 'right', color: '#ccc',
-                maxWidth: 700, backgroundColor: '#111', marginLeft: 'auto', marginRight: 'auto'
-            }}/>
+        <div style={{
+            maxWidth: 700, padding: 8, backgroundColor: '#000',
+            marginLeft: 'auto', marginRight: 'auto', marginTop: 16
+        }}>
+            <TableContainer sx={{height: '78vh', backgroundColor: '#111'}}>
+                <Table stickyHeader>
+                    <LeaderboardHeader/>
 
-            <div style={{
-                maxWidth: 700, padding: '8px 8px 16px 8px', backgroundColor: '#000',
-                marginLeft: 'auto', marginRight: 'auto'
-            }}>
-                <TableContainer sx={{borderRadius: 0, height: '78vh', overflowY: 'scroll', backgroundColor: '#111'}}>
-                    <Table sx={{borderRadius: 0}} stickyHeader>
-                        <LeaderboardHeader/>
+                    <TableBody>
+                        {data.data.map((leader, index) =>
+                            <LeaderboardRow key={leader.id} index={index} leader={leader} user={user}/>
+                        )}
+                    </TableBody>
+                </Table>
+            </TableContainer>
 
-                        <TableBody>
-                            {data.data.map((leader, index) =>
-                                <LeaderboardRow key={leader.id} index={index} leader={leader} user={user}/>
-                            )}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-            </div>
-
-            <div style={{
-                padding: '8px 12px 24px 8px', fontSize: '0.8rem', textAlign: 'right', color: '#ccc',
-                maxWidth: 700, backgroundColor: '#111', marginLeft: 'auto', marginRight: 'auto'
-            }}>
+            <Typography variant='caption' align='right' component='div' style={{width: '100%', marginTop: 8}}>
                 Last updated: {updateTime} GMT
-            </div>
-        </React.Fragment>
+            </Typography>
+        </div>
     )
 }
 
