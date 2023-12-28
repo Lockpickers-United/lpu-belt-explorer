1646cf783da4d851afaca5a54155135cf845eb4d
NiXXeD <NiXXeD@users.noreply.github.com>
2023-12-28 20:40:55 +0000

    deploy: 074a5da6eb8875aa15688810d626b3835bf4981f

--- assets/InfoRoute-4a525e46.js
+++ assets/InfoRoute-4a525e46.js
@@ -1,4 +1,4 @@
-import{j as e,R as t}from"./index-1fee0742.js";import{R as n,U as i,V as a}from"./Nav-444b8237.js";import{C as r,a as s,b as l}from"./CardActions-5f1b1a65.js";const h=`## Earn Lockpicking Karate Flair
+import{j as e,R as t}from"./index-61950255.js";import{R as n,U as i,V as a}from"./Nav-6e56e918.js";import{C as r,a as s,b as l}from"./CardActions-9dc1a6f7.js";const h=`## Earn Lockpicking Karate Flair
 
 As a way to have fun (perhaps at the expense of Six Sigma) this sub gives its members the opportunity to earn user flair in the form of karate belts. Belt flair is issued based on the accomplishments of the member, with each higher level requiring increased community participation and the picking of increasingly difficult locks.
 
