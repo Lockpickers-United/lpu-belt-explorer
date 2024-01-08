9f3db70ab03a5feb7273ea86dda81324230c6d19
mgsecure <125900659+mgsecure@users.noreply.github.com>
2024-01-07 22:12:03 -0500

    feat: add locking mechanism chart to stats page (#36)
    plus some minor responsive refinements

---
+++ src/stats/LockingMechanisms.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import {ResponsivePie} from '@nivo/pie'
+import {pieTheme} from './chartDefaults.js'
+import lockSummaryData from '../data/lockSummaryData.json'
+import useWindowSize from '../util/useWindowSize.jsx'
+
+function LockingMechanisms({}) {
+
+    const data = lockSummaryData.lockingMechanisms
+
+    const {width} = useWindowSize()
+    const smallWidth = width < 500
+    const midWidth = width < 700
+    
+    const myChartHeight = !smallWidth ? 320 : 200
+    const chartMargin = !smallWidth
+        ? {top: 24, right: 0, bottom: 20, left: 0}
+        : {top: 0, right: 80, bottom: 0, left: 120}
+    const arcLinkLabelsSkipAngle = !smallWidth ? 4 : 5
+    const arcLabelsSkipAngle = !smallWidth ? 8 : 10
+    const arcLinkLabelsStraightLength = !smallWidth ? 8 : 0
+
+    return (
+        <React.Fragment>
+            <div key='waffle'
+                 style={{height: myChartHeight, padding: '0px 8px', width: '100%'}}
+            >
+                <ResponsivePie
+                    data={data}
+                    theme={pieTheme}
+                    colors={{scheme: 'dark2'}}
+                    margin={chartMargin}
+                    startAngle={-70}
+                    endAngle={360}
+                    sortByValue={false}
+                    innerRadius={0.5}
+                    padAngle={0.7}
+                    cornerRadius={3}
+                    activeOuterRadiusOffset={8}
+                    arcLinkLabelsSkipAngle={arcLinkLabelsSkipAngle}
+                    arcLinkLabelsTextColor='#ccc'
+                    arcLinkLabelsThickness={2}
+                    arcLinkLabelsColor={{from: 'color'}}
+                    arcLinkLabel={e => e.label + ': ' + e.value + ''}
+                    arcLinkLabelsDiagonalLength={14}
+                    arcLinkLabelsStraightLength={arcLinkLabelsStraightLength}
+                    enableArcLabels={false}
+                    arcLabelsRadiusOffset={0.5}
+                    arcLabelsSkipAngle={arcLabelsSkipAngle}
+                    arcLabelsTextColor='#111'
+                    isInteractive={false}
+                />
+
+            </div>
+        </React.Fragment>
+    )
+}
+
+export default LockingMechanisms
