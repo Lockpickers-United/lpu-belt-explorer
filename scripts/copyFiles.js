455ba7f848a4d1917e3ce9f3df0b98d7697306af
mgsecure <125900659+mgsecure@users.noreply.github.com>
2024-01-11 11:55:33 -0500

    Copy glossary.json to root directory for analysis (#41)

--- scripts/copyFiles.js
+++ scripts/copyFiles.js
@@ -2,3 +2,4 @@ import fs from 'fs'
 
 fs.copyFileSync('./src/data/data.json', './dist/data.json')
 fs.copyFileSync('./src/resources/information.html', './dist/locks/information.html')
+fs.copyFileSync('./src/data/glossary.json', './dist/glossary.json')
