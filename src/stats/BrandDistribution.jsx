d915856f00bb148eacc14ac6162c28929c70e0d4
mgsecure <125900659+mgsecure@users.noreply.github.com>
2024-01-20 17:51:12 -0500

    feat: change brand belts to bar & pie chart for belts/locking mechanism (#49)

---
+++ src/stats/BrandDistribution.jsx
@@ -0,0 +1,108 @@
+import React, {useCallback, useState} from 'react'
+import {FormControl, InputLabel, Select} from '@mui/material'
+import MenuItem from '@mui/material/MenuItem'
+import Divider from '@mui/material/Divider'
+import useWindowSize from '../util/useWindowSize.jsx'
+import BrandBeltBar from './BrandBeltBar.jsx'
+import BrandMechanismPie from './BrandMechanismPie.jsx'
+import statsBrandDistribution from '../data/statsBrandDistribution.json'
+
+function BrandDistribution() {
+
+    const brandData = statsBrandDistribution.brandBelts
+    const mechData = statsBrandDistribution.brandMechanisms
+
+    const brandNameBeltList = brandData.map((brand) => brand.brandName)
+    const [brandIndex, setBrandIndex] = useState(2)
+    const [brandName, setBrandName] = useState(brandData[brandIndex].brandName)
+
+    const brandNameMechList = mechData.map((brand) => brand.brandName)
+    const [brandMechIndex, setBrandMechIndex] = useState(2)
+
+    const beltData = brandData[brandIndex].data
+    const mechanismData = mechData[brandMechIndex].data
+
+    const [open, setOpen] = useState(false)
+    const handleClose = useCallback(() => setOpen(false), [])
+    const handleOpen = useCallback(() => setOpen(true), [])
+
+    const handleChange = useCallback(event => {
+        setBrandIndex(brandNameBeltList.findIndex(x => x === event.target.value))
+        setBrandMechIndex(brandNameMechList.findIndex(x => x === event.target.value))
+        setBrandName(event.target.value)
+        handleClose()
+
+        // TODO: deselect to restore display of 'Brand' header and border?
+        document.getElementById('brandPulldown').classList.remove('active')
+        document.getElementById('brandSelect').classList.remove('active')
+    }, [brandNameBeltList, brandNameMechList, handleClose])
+
+    const {width} = useWindowSize()
+    const mobileSmall = width <= 360
+    const mobileMedium = width <= 395
+    const mobileLarge = width <= 428  // but test also at 412
+    const smallWindow = width <= 560
+
+    const buttonMargin = !smallWindow ? 20 : 10
+
+    const divStyle = {
+        width: '100%', padding: '0px', marginBottom: 12, alignItems: 'center',
+        marginLeft: 'auto', marginRight: 'auto'
+    }
+    const divFlexStyle = !smallWindow ? {display: 'flex'} : {}
+    const combinedDivStyle = {
+        ...divStyle,
+        ...divFlexStyle
+    }
+    const barDivWidth = !smallWindow ? '55%' : '100%'
+    const pieDivWidth = !smallWindow ? '45%' : '100%'
+
+    const barDivHeight = mobileSmall ? 200
+        : smallWindow ? 210
+            :  180
+
+    const pieDivHeight = mobileSmall ? 120
+        : mobileMedium ? 120
+            :  mobileLarge ? 170
+                : smallWindow ? 180
+                    : 160
+
+    return (
+        <React.Fragment>
+            <div style={{marginTop: 24, textAlign: 'center'}}>
+                <FormControl id='brandPulldown' style={{marginBottom: buttonMargin, minWidth: 200, textAlign: 'left'}}>
+                    <InputLabel>Brand</InputLabel>
+                    <Select
+                        id='brandSelect'
+                        value={brandName}
+                        label='Brand'
+                        open={open}
+                        onClose={handleClose}
+                        onOpen={handleOpen}
+                        onChange={handleChange}
+                        style={{fontWeight:700, color:'#eee'}}
+                    >
+                        {brandNameBeltList.map((brand, index) =>
+                            brand === 'Top Brands' && <MenuItem disabled key={index} value={brand}>{brand}</MenuItem>
+                            || brand === 'All Brands' && <MenuItem disabled key={index} value={brand}>{brand}</MenuItem>
+                            || brand === 'divider' && <Divider key={index}/>
+                            || <MenuItem key={index} value={brand}>{brand}</MenuItem>
+                        )}
+                    </Select>
+                </FormControl>
+            </div>
+            <div style={{textAlign: 'center'}}>
+                <div style={combinedDivStyle}>
+                    <div style={{width:barDivWidth, verticalAlign:'top', height:barDivHeight}}>
+                        <BrandBeltBar beltData={beltData} brandName={brandName}/>
+                    </div>
+                    <div style={{width:pieDivWidth, height:pieDivHeight}}>
+                        <BrandMechanismPie beltData={mechanismData} brandName={brandName}/>
+                    </div>
+                </div>
+            </div>
+        </React.Fragment>
+    )
+}
+
+export default BrandDistribution
---
+++ src/stats/BrandMechanismPie.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import {ResponsivePie} from '@nivo/pie'
+import {pieTheme} from './chartDefaults.js'
+import useWindowSize from '../util/useWindowSize.jsx'
+
+function BrandBeltPie({beltData}) {
+
+    const {width} = useWindowSize()
+    const mobileSmall = width <= 360
+    const mobileMedium = width <= 395
+    const mobileLarge = width <= 428  // but test also at 412
+    const smallWindow = width <= 560
+
+    const chartMargin = {top: 6, right: 40, bottom: 20, left: 40}
+
+    const chartHeight = mobileSmall ? 120
+        : mobileMedium ? 140
+            :  mobileLarge ? 180
+                : smallWindow ? 180
+                    : 120
+
+    const arcLinkLabelsSkipAngle = !smallWindow ? 4 : 5
+    const arcLabelsSkipAngle = !smallWindow ? 8 : 10
+    const arcLinkLabelsStraightLength = !smallWindow ? 8 : 0
+
+    const grayColors = ['#949494', '#6b6b6b', '#565656',
+        '#3e3e3e', '#333333', '#2a2a2a']
+
+    return (
+        <div key='pie'
+             style={{height: chartHeight, padding: '0px 0px 0px 0px', width: '100%', backgroundColor:'#000'}}
+        >
+            <ResponsivePie
+                data={beltData}
+                theme={pieTheme}
+                colors={grayColors}
+                margin={chartMargin}
+                startAngle={-90}
+                endAngle={360}
+                sortByValue={false}
+                innerRadius={0.5}
+                padAngle={0.7}
+                cornerRadius={3}
+                activeOuterRadiusOffset={8}
+                arcLinkLabelsSkipAngle={arcLinkLabelsSkipAngle}
+                arcLinkLabelsTextColor='#ccc'
+                arcLinkLabelsThickness={2}
+                arcLinkLabelsColor={{from: 'color'}}
+                arcLinkLabel={e => e.label}
+                arcLinkLabelsDiagonalLength={6}
+                arcLinkLabelsStraightLength={arcLinkLabelsStraightLength}
+                enableArcLabels={false}
+                arcLabelsRadiusOffset={0.5}
+                arcLabelsSkipAngle={arcLabelsSkipAngle}
+                arcLabelsTextColor='#111'
+                isInteractive={false}
+            />
+        </div>
+    )
+}
+
+export default BrandBeltPie
