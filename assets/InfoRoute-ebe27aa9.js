46a89f38bf65e5277dc200783237cd99a06524c9
mgsecure <mgsecure@users.noreply.github.com>
2024-01-20 12:07:03 +0000

    deploy: e22b0468452ccd8d3a1e543575b076205002411b

--- assets/InfoRoute-ebe27aa9.js
+++ assets/InfoRoute-ebe27aa9.js
@@ -1,4 +1,4 @@
-import{j as e,R as t,K as n,N as i,a5 as a,ak as r,al as s}from"./index-efbe8999.js";import{R as l}from"./react-markdown-56ce60a7.js";import"./index-5a2e90bf.js";const h=`## Earn Lockpicking Karate Flair
+import{j as e,R as t,K as n,N as i,a5 as a,ak as r,al as s}from"./index-1fcc0c9d.js";import{R as l}from"./react-markdown-034146ef.js";import"./index-a1b6f908.js";const h=`## Earn Lockpicking Karate Flair
 
 As a way to have fun (perhaps at the expense of Six Sigma) this sub gives its members the opportunity to earn user flair in the form of karate belts. Belt flair is issued based on the accomplishments of the member, with each higher level requiring increased community participation and the picking of increasingly difficult locks.
 
--- assets/PrivacyRoute-8e2746aa.js
+++ assets/PrivacyRoute-8e2746aa.js
@@ -1,4 +1,4 @@
-import{j as e,R as o,K as t,N as n,ak as a,al as r}from"./index-efbe8999.js";import{R as i}from"./react-markdown-56ce60a7.js";import"./index-5a2e90bf.js";const s=`# Privacy Policy
+import{j as e,R as o,K as t,N as n,ak as a,al as r}from"./index-1fcc0c9d.js";import{R as i}from"./react-markdown-034146ef.js";import"./index-a1b6f908.js";const s=`# Privacy Policy
 
 Last updated: November 28, 2023
 
