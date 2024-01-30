d85a265402b644c0c186087ad9bcebc8e3ca94ee
NiXXeD <NiXXeD@users.noreply.github.com>
2024-01-30 21:52:58 +0000

    deploy: d6c0c905069c6a508fbce68911a754cf7ace8383

---
+++ assets/useWindowSize-2eb71cf9.js
@@ -0,0 +1,1 @@
+import{r as n,aE as o,aF as s}from"./index-90635b51.js";function r(){const[e,i]=n.useState({width:0,height:0}),t=()=>{i({width:window.innerWidth,height:window.innerHeight})};return o("resize",t),s(()=>{t()},[]),e}function u(){const{width:e,height:i}=r();return n.useMemo(()=>({width:e,height:i,isMobile:e<650}),[e,i])}export{u};
