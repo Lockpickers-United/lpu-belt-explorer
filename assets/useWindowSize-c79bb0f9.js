57003a4699b3a917a15f13805292ed0324a504f5
mgsecure <mgsecure@users.noreply.github.com>
2024-02-05 16:33:04 +0000

    deploy: ad335d29c11af36342d4fb0684d0c1355ff12119

--- assets/useWindowSize-c79bb0f9.js
+++
@@ -1,1 +0,0 @@
-import{r as n,aF as o,aG as s}from"./index-2ac54035.js";function r(){const[e,i]=n.useState({width:0,height:0}),t=()=>{i({width:window.innerWidth,height:window.innerHeight})};return o("resize",t),s(()=>{t()},[]),e}function u(){const{width:e,height:i}=r();return n.useMemo(()=>({width:e,height:i,isMobile:e<650}),[e,i])}export{u};
