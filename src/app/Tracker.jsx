1551563ed38e73b141bf1fa75f66e7c482cb1834
mgsecure <125900659+mgsecure@users.noreply.github.com>
2024-02-10 08:53:29 -0500

    feat: remove width from tracker query string (called every time window changed, not just once)

--- src/app/Tracker.jsx
+++ src/app/Tracker.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import querystring from 'query-string'
-import useWindowSize from '../util/useWindowSize'
 
 function Tracker({feature, ...extraParams}) {
     if (import.meta.env.DEV) return null
     const randomStuff = (Math.random()).toString(36).substring(2, 10)
     const file = files[feature] || 'lpu.gif'
     const ref = document.referrer || 'none'
-    const {width} = useWindowSize()
-    const query = querystring.stringify({trk: feature, r: randomStuff, w: width, ref, ...extraParams})
+    const query = querystring.stringify({trk: feature, r: randomStuff, ref, ...extraParams})
     const url = `https://images.lpubelts.com/i/${file}?${query}`
     return <img alt='' src={url} width={0} height={0}/>
 }
