d77683b2eb03236a83b46e9bc8dafa12c5ed237d
Andy Christianson <NiXXeD@users.noreply.github.com>
2024-02-11 10:36:17 -0600

    fest: Publish dials.json

--- scripts/copyFiles.js
+++ scripts/copyFiles.js
@@ -3,3 +3,5 @@ import fs from 'fs'
 fs.copyFileSync('./src/data/data.json', './dist/data.json')
 fs.copyFileSync('./src/resources/information.html', './dist/locks/information.html')
 fs.copyFileSync('./src/data/glossary.json', './dist/glossary.json')
+
+fs.copyFileSync('./src/data/dials.json', './dist/dials.json')
