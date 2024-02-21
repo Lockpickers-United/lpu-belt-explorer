1bd1b5955a5fcb9b5b0603fced07680cd4d368f4
Andy Christianson <nix@nixxed.com>
2024-02-21 12:50:39 -0600

    fix: Default to White belt tab.

--- src/locks/LockListContext.jsx
+++ src/locks/LockListContext.jsx
@@ -61,6 +61,7 @@ export function LockListProvider({children}) {
                     return value
                 }
             }
+            return 'White'
         }
 
         return filters.tab
