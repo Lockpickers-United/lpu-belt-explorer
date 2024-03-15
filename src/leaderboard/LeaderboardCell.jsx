708a0b4e3d6740443d61f391fa7d8ef988d8eda3
mgsecure <125900659+mgsecure@users.noreply.github.com>
2024-03-15 18:32:38 -0400

    Make anonymous user darker (#74)

--- src/leaderboard/LeaderboardCell.jsx
+++ src/leaderboard/LeaderboardCell.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import TableCell from '@mui/material/TableCell'
 
 function LeaderboardCell({align = 'center', isCurrentUser, leader, value}) {
-    const color = (leader.displayName ? '#eee' : '#bbb')
+    const color = (leader.displayName !== 'anonymous' ? '#fff' : '#777')
     const style = isCurrentUser
         ? {fontWeight: 600, color: '#4db013'}
         : {fontWeight: 500, color}
