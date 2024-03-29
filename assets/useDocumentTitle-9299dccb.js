50a39b5b92076e40e40cae867c7d3eb1f395ab8a
NiXXeD <NiXXeD@users.noreply.github.com>
2024-03-29 20:53:06 +0000

    deploy: c84c758b2d7827b291b05689871ddde0b07e5a56

---
+++ assets/useDocumentTitle-9299dccb.js
@@ -0,0 +1,1 @@
+import{as as t}from"./index-b31e243f.js";function u(o){t(()=>{window.document.title=o},[o])}export{u};
--- assets/usePageTitle-e3fc7326.js
+++
@@ -1,1 +0,0 @@
-import"./index-2f98bfcc.js";import{u as r}from"./useDocumentTitle-3d1ba395.js";function l(t){const e=t?`LPU Belt Explorer - ${t}`:"LPU Belt Explorer";return r(e)}export{l as u};
