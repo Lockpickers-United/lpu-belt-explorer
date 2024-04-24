983f9febe1ccee8e8c9d9d7a13180b96f2f99a7a
mgsecure <125900659+mgsecure@users.noreply.github.com>
2024-04-24 06:20:15 -0700

    feat: add page param to querystring for error tracking

--- src/app/Tracker.jsx
+++ src/app/Tracker.jsx
@@ -2,13 +2,14 @@ import React, {useContext} from 'react'
 import querystring from 'query-string'
 import AppContext from './AppContext'
 
-function Tracker({feature, ...extraParams}) {
+function Tracker({feature}) {
     const {admin} = useContext(AppContext)
     if (import.meta.env.DEV || admin) return null
     const randomStuff = (Math.random()).toString(36).substring(2, 10)
     const file = files[feature] || 'lpu.gif'
     const ref = document.referrer || 'none'
-    const query = querystring.stringify({trk: feature, r: randomStuff, w: screen.width, ref, ...extraParams})
+    const page = window.location.href.replace(/.*\/#\/(\w+)\?*.*/,'$1')
+    const query = querystring.stringify({trk: feature, r: randomStuff, w: screen.width, ref, page})
     const url = `https://images.lpubelts.com/i/${file}?${query}`
     return <img alt='lpu' src={url} width={0} height={0}/>
 }
