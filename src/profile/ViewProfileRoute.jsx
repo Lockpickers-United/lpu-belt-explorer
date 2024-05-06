456fe8bf7bf9055882872a9a6d1bbb91cbcad77b
mgsecure <125900659+mgsecure@users.noreply.github.com>
2024-05-06 16:34:23 -0400

    feat: generic view profile redirect, v2 (#84)

--- src/profile/ViewProfileRoute.jsx
+++ src/profile/ViewProfileRoute.jsx
@@ -1,17 +1,60 @@
-import {useContext, useEffect} from 'react'
-import {useNavigate} from 'react-router-dom'
+import React, {useContext, useEffect} from 'react'
 import AuthContext from '../app/AuthContext'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import CardHeader from '@mui/material/CardHeader'
+import Footer from '../nav/Footer'
+import LinearProgress from '@mui/material/LinearProgress'
+import Nav from '../nav/Nav'
+import Tracker from '../app/Tracker'
+import Typography from '@mui/material/Typography'
+import lpuLogoPath from '../resources/LPU.png'
+import {useDocumentTitle} from 'usehooks-ts'
+import {useNavigate} from 'react-router-dom'
 
 function ViewProfileRoute() {
-    const {authLoaded, user} = useContext(AuthContext)
+    const {authLoaded, isLoggedIn, user} = useContext(AuthContext)
     const navigate = useNavigate()
 
-    useEffect(() => {
-        if (authLoaded && user) navigate(`/profile/${user.uid}`)
-        else if (authLoaded && !user) navigate('/locks')
-    }, [navigate, authLoaded, user])
+    useDocumentTitle('LPU Belt Explorer - View Profile')
 
-    return null
+    useEffect(() => {
+        if (authLoaded && user) {
+            navigate(`/profile/${user.uid}`)
+        }
+    }, [authLoaded, navigate, user])
+
+    return (
+        <React.Fragment>
+            <Nav title='View Profile'/>
+            {!authLoaded &&
+                <React.Fragment>
+                    <LinearProgress variant='indeterminate' color='secondary'/>
+                    <img alt='Loading' src={lpuLogoPath} style={{
+                        marginLeft: 'auto', marginRight: 'auto', display: 'block'
+                    }}/>
+                </React.Fragment>
+            }
+            {authLoaded && !isLoggedIn &&
+                <Card style={{
+                    marginTop: 16,
+                    maxWidth: 350,
+                    marginLeft: 'auto',
+                    marginRight: 'auto',
+                    borderRadius: 0
+                }}>
+                    <CardHeader title='Please Log In'/>
+                    <CardContent>
+                        <Typography variant='h6' align='center'>
+                            You must be logged in to view your profile.
+                        </Typography>
+                    </CardContent>
+                </Card>
+            }
+            <Footer/>
+            <Tracker feature='viewprofileredirect'/>
+        </React.Fragment>
+    )
 }
 
 export default ViewProfileRoute
