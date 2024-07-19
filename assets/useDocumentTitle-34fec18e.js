9fde7753bf88e6fff9826be6331ff0f13c3c46a2
NiXXeD <NiXXeD@users.noreply.github.com>
2024-07-19 15:08:27 +0000

    deploy: 1aca746162f2431abc3eb849b0d40c079b999f2a

--- assets/useDocumentTitle-34fec18e.js
+++
@@ -1,1 +0,0 @@
-import{aH as t}from"./index-a9ad2893.js";function u(o){t(()=>{window.document.title=o},[o])}export{u};
---
+++ assets/useDocumentTitle-984a2642.js
@@ -0,0 +1,1 @@
+import{aH as t}from"./index-fe3cdc37.js";function u(o){t(()=>{window.document.title=o},[o])}export{u};
