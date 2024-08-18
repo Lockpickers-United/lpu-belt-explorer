5b87a49844850124403e520fe1f91839dc6639db
accardi <accardi@gmail.com>
2024-08-17 19:21:18 -0600

    exclude dev.lpubelts.com

--- src/data/serverEnv.js
+++ src/data/serverEnv.js
@@ -1,1 +1,1 @@
-export const production = location.host.endsWith('lpubelts.com') && !import.meta.env.DEV
+export const production = location.host.endsWith('lpubelts.com') && location.host !== 'dev.lpubelts.com' && !import.meta.env.DEV
