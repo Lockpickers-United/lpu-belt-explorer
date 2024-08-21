010a884bc3cc13bf89f18b5f8c8688d3226c00e6
mgsecure <125900659+mgsecure@users.noreply.github.com>
2024-08-21 00:27:00 -0400

    feat: Leaderboard Compare Black Belts. (#93)
    * compare mode initial (data from leaderboard json)
    
    * compare mode cleanup

---
+++ src/leaderboard/CompareBeltBar.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import {primaryTheme} from '../stats/chartDefaults'
+import useWindowSize from '../util/useWindowSize'
+import {ResponsiveBar} from '@nivo/bar'
+import {danBelts} from '../data/belts'
+
+function CompareBeltBar({userData, max}) {
+
+    if (!userData) return null
+
+    const beltData = danBelts.reduce((acc, belt) => {
+        const count = userData['beltCounts'][belt] ? userData['beltCounts'][belt] : 0
+        acc.push({id: belt, label: belt, count: count, value: count})
+        return acc
+    }, [])
+
+
+    const {width} = useWindowSize()
+    const smallWindow = width <= 560
+
+    const chartHeight = !smallWindow ? 225
+        : 180
+
+    const chartMargin = !smallWindow
+        ? {top: 0, right: 0, bottom: 60, left: 15}
+        : {top: 0, right: 0, bottom: 60, left: 15}
+
+    const beltColors =
+        ['#d5d5d5', '#d8d801', '#ed7d01', '#389700',
+            '#0090de', '#634b9f', '#9d5918',
+            '#ba0303', '#000', '#000', '#000',
+            '#000', '#000', '#a5a93c']
+
+    const labelColors =
+        ['#000', '#000', '#000', '#ddd',
+            '#ddd', '#ddd', '#ddd',
+            '#ddd', '#ddd', '#ddd', '#ddd',
+            '#ddd', '#ddd', '#ddd', ]
+
+    return (
+        <div key='bar'
+             style={{height: chartHeight, padding: '0px 8px 0px 8px', width: '100%'}}
+        >
+            <ResponsiveBar
+                data={beltData}
+                maxValue={max}
+                margin={chartMargin}
+                padding={0.15}
+                colors={(bar) => beltColors[bar.index % beltColors.length]}
+                animate={true}
+                axisBottom={{
+                    tickRotation: -45,
+                }}
+                axisLeft={null}
+                enableGridY={false}
+                theme={primaryTheme}
+                isInteractive={false}
+                enableLabel={true}
+                label={e => e.data.count}
+                labelSkipWidth={12}
+                labelSkipHeight={12}
+                labelTextColor={(bar) => labelColors[bar.index % labelColors.length]}
+            />
+        </div>
+    )
+}
+
+export default CompareBeltBar
--- src/leaderboard/LeaderboardName.jsx
+++ src/leaderboard/LeaderboardName.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import makeStyles from '@mui/styles/makeStyles'
+import {useSearchParams} from 'react-router-dom'
 
 function LeaderboardName({leader, isCurrentUser}) {
+
+    const [searchParams] = useSearchParams()
+    const tab = searchParams.get('tab')
+
     const classes = useStyles()
-    const style = isCurrentUser ? {color:'#4db013'} : {}
+    const style = isCurrentUser ? {color: '#4db013'} : {}
 
     if (leader.displayName) {
         const safeName = leader.displayName.replace(/\s/g, '_')
-        const href = `/#/profile/${leader.id}?name=${safeName}`
+        const href = tab === 'blackBelts'
+            ? `/#/profile/${leader.id}/scorecard?name=${safeName}`
+            : tab === 'safelocks'
+                ? `/#/profile/${leader.id}/safelocks?name=${safeName}`
+                : `/#/profile/${leader.id}?name=${safeName}`
+
         return <a className={classes.name} style={style} href={href}>{leader.displayName}</a>
     } else {
         return 'Anonymous'
