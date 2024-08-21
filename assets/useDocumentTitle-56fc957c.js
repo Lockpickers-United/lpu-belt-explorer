7b8364d25c76025f0a3c8f27d26361a97a377017
accardi <accardi@users.noreply.github.com>
2024-08-21 16:35:03 +0000

    deploy: 6a9f8e515e5ad397349db67cfa29dbeed9d0c219

---
+++ assets/useDocumentTitle-56fc957c.js
@@ -0,0 +1,1 @@
+import{bl as t}from"./index-c3f5272d.js";function u(o){t(()=>{window.document.title=o},[o])}export{u};
--- assets/usePageTitle-558c6360.js
+++
@@ -1,1 +0,0 @@
-import"./index-7b1bb14e.js";import{u as r}from"./useDocumentTitle-4e5a3b1c.js";function l(t){const e=t?`LPU Belt Explorer - ${t}`:"LPU Belt Explorer";return r(e)}export{l as u};
