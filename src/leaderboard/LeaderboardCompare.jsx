057bc08337dfb1b545e11c287246e2c0360f7684
mgsecure <125900659+mgsecure@users.noreply.github.com>
2024-08-21 15:31:38 -0700

    feat: leaderboard compare - sharable url

--- src/leaderboard/LeaderboardCompare.jsx
+++ src/leaderboard/LeaderboardCompare.jsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useState} from 'react'
+import React, {useCallback, useMemo, useState} from 'react'
 import {danBelts} from '../data/belts'
 import CompareBeltBar from './CompareBeltBar.jsx'
 import CompareDanStats from './CompareDanStats.jsx'
@@ -8,13 +8,43 @@ import AccordionSummary from '@mui/material/AccordionSummary'
 import CompareSelect from './CompareSelect.jsx'
 import Button from '@mui/material/Button'
 import useWindowSize from '../util/useWindowSize.jsx'
+import {useLocation, useSearchParams} from 'react-router-dom'
+import queryString from 'query-string'
 
 function LeaderboardCompare({blackBeltData}) {
+    const location = useLocation()
+    const [searchParams, setSearchParams] = useSearchParams()
 
-    const [open, setOpen] = useState(false)
-    const [fighter1, setFighter1] = useState('')
-    const [fighter2, setFighter2] = useState('')
+    const {tab, bb1, bb2, title} = useMemo(() => {
+        const query = queryString.parse(location.search)
+        return {
+            tab: query.tab ? query.tab : undefined,
+            bb1: query.bb1 ? query.bb1 : undefined,
+            bb2: query.bb2 ? query.bb2 : undefined,
+            title: query.title ? query.title : undefined
+        }
+    }, [location.search])
+
+    const fighter1 = blackBeltData.find(bb => bb.id === bb1) || ''
+    const fighter2 = blackBeltData.find(bb => bb.id === bb2) || ''
+
+    const [open, setOpen] = useState(!!bb1 || !!bb2)
     const handleClick = useCallback(() => setOpen(!open), [open])
+    const handleReset = useCallback(() => {
+        searchParams.delete('bb1')
+        searchParams.delete('bb2')
+        searchParams.delete('title')
+        setSearchParams(searchParams)
+        setOpen(!open)
+    }, [searchParams, setSearchParams, open])
+
+    const name1 = fighter1?.displayName ? fighter1.displayName.replace(/[\s/]/g, '_').replace(/\W/g, '') : 'Unknown'
+    const name2 = fighter2?.displayName ? fighter2.displayName.replace(/[\s/]/g, '_').replace(/\W/g, '') : 'Unknown'
+    const safeNames = (!!bb1 || !!bb2) ? name1 + '-vs-' + name2 : null
+
+    if ((!!bb1 || !!bb2) && (title !== safeNames)) {
+        setTimeout(() => setSearchParams({'tab': tab, 'bb1': bb1, 'bb2': bb2, 'title': safeNames}), 250)
+    }
 
     const maxValue = danBelts.reduce((acc, belt) => {
         const value1 = fighter1 && fighter1['beltCounts'][belt] ? fighter1['beltCounts'][belt] : 0
@@ -34,7 +64,6 @@ function LeaderboardCompare({blackBeltData}) {
     const cardWidth = !isMobile ? '50%' : '100%'
     const nameMargin = !isMobile ? 10 : 0
 
-
     return (
         <React.Fragment>
             <Accordion expanded={open} disableGutters={true} style={{marginBottom: 20, backgroundColor: bgcolor}}>
@@ -45,20 +74,31 @@ function LeaderboardCompare({blackBeltData}) {
                     backgroundColor: bgcolor
                 }}>
                     <div style={{width: '100%', textAlign: 'center'}}>
-                        <Button onClick={handleClick} variant='outlined' color='info' size='small'>Compare
-                            Mode!</Button>
+                        {!open &&
+                            <Button onClick={handleClick} variant='outlined' color='info' size='small'>
+                                Compare Mode!
+                            </Button>
+                        }
+                        {open &&
+                            <React.Fragment>
+                                <Button onClick={handleReset} variant='outlined' color='info' size='small'
+                                        style={{marginLeft: 10}}>
+                                    Close
+                                </Button>
+                            </React.Fragment>
+                        }
                     </div>
                 </AccordionSummary>
                 <AccordionDetails style={{backgroundColor: '#111', padding: '8px 0px 0px 0px'}}>
                     <div style={{backgroundColor: '#111', padding: 0}}>
                         <div style={{display: 'flex'}}>
                             <div style={{width: '50%', padding: 0, margin: 4}}>
-                                <CompareSelect blackBeltData={blackBeltData} fighter={fighter1} setFighter={setFighter1}
-                                               label={'Fighter #1'}/>
+                                <CompareSelect blackBeltData={blackBeltData} fighter={fighter1}
+                                               label={'Fighter #1'} param={'bb1'}/>
                             </div>
                             <div style={{width: '50%', padding: 0, margin: 4}}>
-                                <CompareSelect blackBeltData={blackBeltData} fighter={fighter2} setFighter={setFighter2}
-                                               label={'Fighter #2'}/>
+                                <CompareSelect blackBeltData={blackBeltData} fighter={fighter2}
+                                               label={'Fighter #2'} param={'bb2'}/>
                             </div>
                         </div>
 
