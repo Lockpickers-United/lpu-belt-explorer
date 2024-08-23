daac648ff28db9d020ca487de345d1cb87546a9d
mgsecure <mgsecure@users.noreply.github.com>
2024-08-23 04:40:02 +0000

    deploy: e78bdba77f1e2a634b272354f0f03377c424dae9

--- assets/useDocumentTitle-23576d89.js
+++
@@ -1,1 +0,0 @@
-import{bm as t}from"./index-98448aa4.js";function u(o){t(()=>{window.document.title=o},[o])}export{u};
---
+++ assets/usePageTitle-5ee83162.js
@@ -0,0 +1,1 @@
+import"./index-b57827ec.js";import{u as r}from"./useDocumentTitle-affc891b.js";function l(t){const e=t?`LPU Belt Explorer - ${t}`:"LPU Belt Explorer";return r(e)}export{l as u};
