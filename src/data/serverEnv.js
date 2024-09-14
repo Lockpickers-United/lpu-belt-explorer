2801de75e89686b2a29f07a08abdc49097a4eea8
mgsecure <125900659+mgsecure@users.noreply.github.com>
2024-09-14 12:02:48 -0700

    restore serverEnv

--- src/data/serverEnv.js
+++ src/data/serverEnv.js
@@ -1,1 +1,1 @@
-export const production = (location.host.endsWith('lpubelts.com') && location.host !== 'dev.lpubelts.com' && !import.meta.env.DEV) || true
+export const production = (location.host.endsWith('lpubelts.com') && location.host !== 'dev.lpubelts.com' && !import.meta.env.DEV)
