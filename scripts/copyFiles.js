1da26a9cd70e0e8acbfd146cab3a5e73d59396fa
mgsecure <125900659+mgsecure@users.noreply.github.com>
2024-11-19 12:23:37 -0500

    create build-dev-import for yarn

--- scripts/copyFiles.js
+++ scripts/copyFiles.js
@@ -1,9 +1,9 @@
 import fs from 'fs'
 
 fs.copyFileSync('./src/data/data.json', './dist/data.json')
-fs.copyFileSync('./src/resources/information.html', './dist/locks/information.html')
+//fs.copyFileSync('./src/resources/information.html', './dist/locks/information.html')
 fs.copyFileSync('./src/data/glossary.json', './dist/glossary.json')
-
 fs.copyFileSync('./src/data/safelocks.json', './dist/dials.json')
 fs.copyFileSync('./src/data/projects.json', './dist/projects.json')
 fs.copyFileSync('./src/data/awards.json', './dist/awards.json')
+fs.copyFileSync('./src/data/rafl.json', './dist/rafl.json')
