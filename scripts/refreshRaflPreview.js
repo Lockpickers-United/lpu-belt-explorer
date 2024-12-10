2f71d3cb9b51c3b08e9afdc81aab504de24f0633
mgsecure <125900659+mgsecure@users.noreply.github.com>
2024-12-10 09:19:11 -0800

    forgot to add it back in to package.json

--- scripts/refreshRaflPreview.js
+++ scripts/refreshRaflPreview.js
@@ -1,9 +1,5 @@
 import fetch from 'node-fetch'
-
-const url = process.env.USER !== 'testing'
-    ? 'http' + '://explore.lpubelts.com:8080/refresh-preview'
-    : 'https://explore.lpubelts.com:8443/refresh-preview'
-
+const url = 'http' + '://explore.lpubelts.com:8080/refresh-preview'
 const refreshResults = await (await fetch(url)).json()
 
 console.log('Refresh Raffle Preview Results:', refreshResults)
