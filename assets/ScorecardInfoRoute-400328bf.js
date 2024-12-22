d9e31f11987e5c10b9cdf2f1daf3e9c3d43474bd
mgsecure <mgsecure@users.noreply.github.com>
2024-12-22 17:00:07 +0000

    deploy: 099a8998a2bdb55b8191469eb2e9a3e3e67148dd

---
+++ assets/ScorecardInfoRoute-400328bf.js
@@ -0,0 +1,1 @@
+import{j as r,R as e,N as a,$ as o,F as t,T as s}from"./index-c559e00f.js";import{u as c}from"./usePageTitle-86bb7bb3.js";import"./useDocumentTitle-08fd66e1.js";function u(){return c("Scorecard Info"),r.jsxs(e.Fragment,{children:[r.jsx(a,{title:"Scorecard Info"}),r.jsx(o,{}),r.jsx(t,{}),r.jsx(s,{feature:"scorecard-info"})]})}export{u as default};
---
+++ assets/usePageTitle-86bb7bb3.js
@@ -0,0 +1,1 @@
+import"./index-c559e00f.js";import{u as n}from"./useDocumentTitle-08fd66e1.js";function s(t){const o=/^localhost.*/.test(window.location.host)?"LOCAL":/^dev.*/.test(window.location.host)?"DEV":"LPU Belt Explorer",e=t?`${o} - ${t}`:"LPU Belt Explorer";return n(e)}export{s as u};
