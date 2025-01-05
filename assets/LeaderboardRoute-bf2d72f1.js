fb4edb6bd6138c7e21ad7a4b304d47e7cf0717e6
mgsecure <mgsecure@users.noreply.github.com>
2025-01-05 19:47:22 +0000

    deploy: 550e4732b670c94e334e52713901e5558f237f04

--- assets/LeaderboardRoute-bf2d72f1.js
+++
@@ -1,1 +0,0 @@
-import{j as e,R as r,$ as a,F as t,T as s}from"./index-adcc3ca0.js";import{u as o}from"./usePageTitle-89ca3d06.js";import"./useDocumentTitle-b661c74a.js";function m(){return o("Leaderboard"),e.jsxs(r.Fragment,{children:[e.jsx(a,{}),e.jsx(t,{}),e.jsx(s,{feature:"leaderboard"})]})}export{m as default};
---
+++ assets/useDocumentTitle-280e2d27.js
@@ -0,0 +1,1 @@
+import{bk as t}from"./index-dcadc41d.js";function u(o){t(()=>{window.document.title=o},[o])}export{u};
