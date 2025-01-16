304020f84c4b3102a74c4b54cc2f5cdab72fed46
mgsecure <mgsecure@users.noreply.github.com>
2025-01-16 17:32:42 +0000

    deploy: fe3b4bf4dbbe42bedc3e2ae6664b392bc982e2f1

--- assets/LoadingDisplay-a3585a7c.js
+++
@@ -1,1 +0,0 @@
-import{j as a,R as r}from"./index-642d67a2.js";import{l as o}from"./LPU-c3fa7122.js";import{L as t}from"./LinearProgress-ab3b9e28.js";function e(){return a.jsxs(r.Fragment,{children:[a.jsx(t,{variant:"indeterminate",color:"secondary"}),a.jsx("img",{alt:"Loading",src:o,style:{marginLeft:"auto",marginRight:"auto",display:"block"}})]})}export{e as L};
--- assets/useDocumentTitle-5b546868.js
+++
@@ -1,1 +0,0 @@
-import{bk as t}from"./index-642d67a2.js";function u(o){t(()=>{window.document.title=o},[o])}export{u};
