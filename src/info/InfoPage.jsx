a7c9ce65a75c2a30851b67034c6228b0fdc8443b
mgsecure <125900659+mgsecure@users.noreply.github.com>
2025-02-08 16:03:33 -0800

    belt requirements updates: TOC & Export page

--- src/info/InfoPage.jsx
+++ src/info/InfoPage.jsx
@@ -1,17 +1,62 @@
+import React from 'react'
 import Card from '@mui/material/Card'
 import CardActions from '@mui/material/CardActions'
 import CardContent from '@mui/material/CardContent'
 import CardHeader from '@mui/material/CardHeader'
-import React from 'react'
+import GithubButton from '../nav/GithubButton'
+import Link from '@mui/material/Link'
+
 import ReactMarkdown from 'react-markdown'
 import rehypeExternalLinks from 'rehype-external-links'
-import GithubButton from '../nav/GithubButton'
 import introMd from '../resources/intro.md?raw'
 import infoMd from '../resources/info.md?raw'
 import changelogMd from '../resources/changelog.md?raw'
 
 function InfoPage() {
-    const updateTime = '10/20/2024'
+    const updateTime = '02/10/2025'
+
+    const MarkdownRenderer = ({content}) => {
+        return (
+            <ReactMarkdown
+                remarkPlugins={[]}
+                rehypePlugins={[[rehypeExternalLinks, {
+                    target: '_blank',
+                    rel: ['nofollow', 'noopener', 'noreferrer']
+                }]]}
+                components={{
+                    h1: ({node, ...props}) => <h1
+                        id={node.children[0].value.toLowerCase().replace(/ /g, '-')} {...props} />,
+                    h2: ({node, ...props}) => <h2
+                        id={node.children[0].value.toLowerCase().replace(/ /g, '-')} {...props} />,
+                    h3: ({node, ...props}) => <h3
+                        id={node.children[0].value.toLowerCase().replace(/ /g, '-')} {...props} />,
+                    h4: ({node, ...props}) => <h4
+                        id={node.children[0].value.toLowerCase().replace(/ /g, '-')} {...props} />,
+                    h5: ({node, ...props}) => <h5
+                        id={node.children[0].value.toLowerCase().replace(/ /g, '-')} {...props} />,
+                    h6: ({node, ...props}) => <h6
+                        id={node.children[0].value.toLowerCase().replace(/ /g, '-')} {...props} />
+                }}>{content}
+            </ReactMarkdown>
+        )
+    }
+
+    const sections = ['Earn Lockpicking Karate Flair', 'Belt Request Instructions', 'FAQ', 'General Rules', 'Video Requirements', 'Blue Belt Project Requirements', 'SFIC Filming Requirements', 'Changes to Lock Rankings']
+    const toc = sections.map(section => {
+        const id = section.toLowerCase().replace(/ /g, '-')
+        return (
+            <li key={id}><Link onClick={() => scrollIntoView(id)}
+                               style={{color: '#fff', textDecoration: 'none', cursor: 'pointer'}}>{section}</Link></li>
+        )
+    })
+
+    const scrollIntoView = (id) => {
+        document.getElementById(id).scrollIntoView({behavior: 'smooth', block: 'start'})
+        document.getElementById(id).scrollBy({   // doesn't work
+            top: -100,
+            left: 0
+        })
+    }
 
     return (
         <React.Fragment>
@@ -20,15 +65,18 @@ function InfoPage() {
                 marginLeft: 'auto',
                 marginRight: 'auto',
                 marginTop: 16,
-                marginButtom: 16
+                marginBottom: 16
             }}>
                 <CardHeader title='Belt Requirements' action={
-                    <GithubButton url='https://github.com/Lockpickers-United/lpu-belt-explorer/blob/main/src/resources/info.md'/>
+                    <GithubButton
+                        url='https://github.com/Lockpickers-United/lpu-belt-explorer/blob/main/src/resources/info.md'/>
                 }/>
                 <CardContent>
-                    <ReactMarkdown rehypePlugins={[[rehypeExternalLinks, {target: '_blank'}]]}>
-                        {markdown}
-                    </ReactMarkdown>
+
+                    <h2>Table of Contents</h2>
+                    <ul>{toc}</ul>
+                    <MarkdownRenderer content={markdown}/>
+
                 </CardContent>
                 <CardActions>
                     Updated: {updateTime}
