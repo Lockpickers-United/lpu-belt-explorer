e9ea3059d88fb2bf4abd5fadefbb42245a0cb6c7
mgsecure <mgsecure@users.noreply.github.com>
2025-05-21 16:41:11 +0000

    deploy: 3e266d91fbd71f9727bf6daddd30ae0cd8003f81

---
+++ assets/ScorecardInfoRoute-a9735dc3.js
@@ -0,0 +1,1 @@
+import{j as r,R as e,N as a,a1 as o,h as t,T as s}from"./index-84fa0513.js";import{u as c}from"./usePageTitle-bfbb7477.js";import"./useDocumentTitle-cd559466.js";function u(){return c("Scorecard Info"),r.jsxs(e.Fragment,{children:[r.jsx(a,{title:"Scorecard Info"}),r.jsx(o,{}),r.jsx(t,{}),r.jsx(s,{feature:"scorecard-info"})]})}export{u as default};
---
+++ assets/useDocumentTitle-cd559466.js
@@ -0,0 +1,1 @@
+import{bf as t}from"./index-84fa0513.js";function u(o){t(()=>{window.document.title=o},[o])}export{u};
