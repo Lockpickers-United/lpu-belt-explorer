8b04fbfb2ef09698a45aa310d75dcfb919dddfaf
mgsecure <mgsecure@users.noreply.github.com>
2025-06-02 19:45:45 +0000

    deploy: d14b43bfd37e7bea8903b504ab43b08ff4d4c009

--- assets/useDocumentTitle-35ee003f.js
+++
@@ -1,1 +0,0 @@
-import{bf as t}from"./index-22799da5.js";function u(o){t(()=>{window.document.title=o},[o])}export{u};
---
+++ assets/useDocumentTitle-71c12abf.js
@@ -0,0 +1,1 @@
+import{bf as t}from"./index-0c755afe.js";function u(o){t(()=>{window.document.title=o},[o])}export{u};
