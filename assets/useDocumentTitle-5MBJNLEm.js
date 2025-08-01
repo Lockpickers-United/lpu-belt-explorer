79c188272d77d97fa17a9bcc9fe8c4db9110edd5
mgsecure <mgsecure@users.noreply.github.com>
2025-08-01 15:37:37 +0000

    deploy: 44ae3dfcd69ac61dea45633520f55e4b5ee7ccf7

--- assets/useDocumentTitle-5MBJNLEm.js
+++
@@ -1,1 +0,0 @@
-import{bf as t}from"./index-BM4DZBjA.js";function u(o){t(()=>{window.document.title=o},[o])}export{u};
--- assets/usePageTitle-BFPnHQuX.js
+++
@@ -1,1 +0,0 @@
-import"./index-BM4DZBjA.js";import{u as e}from"./useDocumentTitle-5MBJNLEm.js";function l(t){return e(n(t))}function n(t){const o=/^localhost.*/.test(window.location.host)?"LOCAL":/^dev.*/.test(window.location.host)?"DEV":"LPU Belt Explorer";return t?`${o} - ${t}`:"LPU Belt Explorer"}export{l as u};
