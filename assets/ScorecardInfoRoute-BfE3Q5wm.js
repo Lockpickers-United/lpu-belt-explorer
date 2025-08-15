7af895a69d5933e19b66549fda45eb02e287d170
mgsecure <mgsecure@users.noreply.github.com>
2025-08-15 19:43:00 +0000

    deploy: dd4036f1dea3b190df99fbfc9d6f9762fcc3a764

---
+++ assets/ScorecardInfoRoute-BfE3Q5wm.js
@@ -0,0 +1,1 @@
+import{j as r,R as e,N as a,a3 as o,h as s,T as t}from"./index-D6MTDNWa.js";import{u as c}from"./usePageTitle-CfGjmWIS.js";function u(){return c("Scorecard Info"),r.jsxs(e.Fragment,{children:[r.jsx(a,{title:"Scorecard Info"}),r.jsx(o,{}),r.jsx(s,{}),r.jsx(t,{feature:"scorecard-info"})]})}export{u as default};
