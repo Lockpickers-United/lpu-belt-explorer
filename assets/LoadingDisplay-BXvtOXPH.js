ea56fc2516a18dcff3d5ad6d5e03e03c3394d571
mgsecure <mgsecure@users.noreply.github.com>
2025-08-28 22:14:30 +0000

    deploy: a2a8f409cbb5a4ac6f643a8e101e57551abc3033

--- assets/LoadingDisplay-BXvtOXPH.js
+++
@@ -1,1 +0,0 @@
-import{j as a,R as r}from"./index-KX6fSY5H.js";import{l as o}from"./LPU-DltM2OBF.js";import{L as t}from"./LinearProgress-Be5DIAmZ.js";function e(){return a.jsxs(r.Fragment,{children:[a.jsx(t,{variant:"indeterminate",color:"secondary"}),a.jsx("img",{alt:"Loading",src:o,style:{marginLeft:"auto",marginRight:"auto",display:"block"}})]})}export{e as L};
--- assets/ScorecardInfoRoute-DCzK50O1.js
+++
@@ -1,1 +0,0 @@
-import{j as r,R as e,N as a,a3 as o,h as s,T as t}from"./index-KX6fSY5H.js";import{u as c}from"./usePageTitle-6QUGex1F.js";function u(){return c("Scorecard Info"),r.jsxs(e.Fragment,{children:[r.jsx(a,{title:"Scorecard Info"}),r.jsx(o,{}),r.jsx(s,{}),r.jsx(t,{feature:"scorecard-info"})]})}export{u as default};
