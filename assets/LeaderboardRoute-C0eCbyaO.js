08a8fd257c06d72f28e2937043ac0dc029b1fe19
mgsecure <mgsecure@users.noreply.github.com>
2025-10-03 17:09:56 +0000

    deploy: 1cc1d835970447f090bd00d905c96bc52f0e0edc

---
+++ assets/LeaderboardRoute-C0eCbyaO.js
@@ -0,0 +1,1 @@
+import{j as e,R as a,a3 as r,h as t,T as s}from"./index-Bwxc9M6K.js";import{u as o}from"./usePageTitle-BoQBCE1z.js";function x(){return o("Leaderboard"),e.jsxs(a.Fragment,{children:[e.jsx(r,{}),e.jsx(t,{}),e.jsx(s,{feature:"leaderboard"})]})}export{x as default};
--- assets/ProfileNotFound-DvhA_A5i.js
+++
@@ -1,1 +0,0 @@
-import{j as a,a4 as t,am as o,a5 as n,M as i}from"./index-B9NgfWfg.js";function s(){const r={marginTop:16,maxWidth:350,marginLeft:"auto",marginRight:"auto",borderRadius:0};return a.jsxs(t,{style:r,children:[a.jsx(o,{title:"Profile Not Found!"}),a.jsx(n,{children:a.jsx(i,{variant:"h6",align:"center",children:"This profile was not found."})})]})}export{s as P};
