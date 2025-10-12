1da8a95b7c11703d375aba3cf6efcc788c732f7a
mgsecure <mgsecure@users.noreply.github.com>
2025-10-12 21:47:07 +0000

    deploy: 95f92010170125ffbcab8e9367b348bf1dc9c867

--- assets/LoadingDisplay-CZugUNs6.js
+++
@@ -1,1 +0,0 @@
-import{j as a,R as r}from"./index-Jkc3F5C3.js";import{l as o}from"./LPU-DltM2OBF.js";import{L as t}from"./LinearProgress-DuDfK8HX.js";function e(){return a.jsxs(r.Fragment,{children:[a.jsx(t,{variant:"indeterminate",color:"secondary"}),a.jsx("img",{alt:"Loading",src:o,style:{marginLeft:"auto",marginRight:"auto",display:"block"}})]})}export{e as L};
