e041ecb2caae6bc3b640682b580552314b026ebc
mgsecure <mgsecure@users.noreply.github.com>
2025-10-13 19:54:06 +0000

    deploy: 2158c585e708d5c4cafa65dad4febd785b031d51

--- assets/LeaderboardRoute-B1EQHQ8q.js
+++
@@ -1,1 +0,0 @@
-import{j as e,R as a,ad as r,h as t,T as s}from"./index-BfcAGizo.js";import{u as o}from"./usePageTitle-BdRXR1Rx.js";function x(){return o("Leaderboard"),e.jsxs(a.Fragment,{children:[e.jsx(r,{}),e.jsx(t,{}),e.jsx(s,{feature:"leaderboard"})]})}export{x as default};
