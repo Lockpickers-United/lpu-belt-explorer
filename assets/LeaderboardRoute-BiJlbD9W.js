7e7250cf908ffcbfac7d0d58149e05d98ed065ca
mgsecure <mgsecure@users.noreply.github.com>
2025-10-15 17:45:55 +0000

    deploy: c3f897f4dec31f31ed13dee85df518e622feeae4

---
+++ assets/LeaderboardRoute-BiJlbD9W.js
@@ -0,0 +1,1 @@
+import{j as e,R as a,a9 as r,h as t,T as s}from"./index-BaQvk_ff.js";import{u as o}from"./usePageTitle-RADnKllL.js";function x(){return o("Leaderboard"),e.jsxs(a.Fragment,{children:[e.jsx(r,{}),e.jsx(t,{}),e.jsx(s,{feature:"leaderboard"})]})}export{x as default};
