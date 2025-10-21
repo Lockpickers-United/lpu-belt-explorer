33545d735ff559a97030a30a5f71ece16550bc15
mgsecure <mgsecure@users.noreply.github.com>
2025-10-21 13:54:05 +0000

    deploy: b142f123d424330f53052748ed21597c3f6944c3

---
+++ assets/LoadingDisplay-BNTD3ATC.js
@@ -0,0 +1,1 @@
+import{j as a,R as r}from"./index--p5yIcos.js";import{l as o}from"./LPU-DltM2OBF.js";import{L as t}from"./LinearProgress-DbxbxKnr.js";function e(){return a.jsxs(r.Fragment,{children:[a.jsx(t,{variant:"indeterminate",color:"secondary"}),a.jsx("img",{alt:"Loading",src:o,style:{marginLeft:"auto",marginRight:"auto",display:"block"}})]})}export{e as L};
