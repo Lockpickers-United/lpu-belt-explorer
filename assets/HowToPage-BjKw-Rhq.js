8f4a1295d00735fedb379068b1142020e38e3bd5
mgsecure <mgsecure@users.noreply.github.com>
2025-10-26 20:37:47 +0000

    deploy: 577473cb141b664e949b114a52ebc0aa4c658536

---
+++ assets/HowToPage-BjKw-Rhq.js
@@ -0,0 +1,102 @@
+import{r,A as s,j as e,R as i,ab as l,ac as h,as as c}from"./index-CR79cG60.js";/* empty css                  */import{M as d}from"./index-ne67chMJ.js";import{r as u}from"./index-Bx14Hi_Z.js";import{r as p}from"./index-Bx_Wt7DU.js";const y=`## Hello Black Belts!
+
+In building towards a vision where core picking activities are integrated in one place, the Dan System is now also available on lpubelts.com. We hope Scorecard will become the official successor to the Dan Sheet but for now both systems are active and can be used for tracking your picks and projects as well as requesting Dan Levels through #belt-requests.
+
+Worth noting: because the Dan Sheet cannont be actively connected to the Dan Sheet, changes in one place are not reflected in the other. It’s best to choose one or the other though of course you can keep both up-to-date. All of your lock and project additions can be made on lpubelts.com, and Dan Requests would reference your lpubelts.com Scorecard.
+
+Follow these instructions to migrate your dan sheet to lpubelts.com:
+
+1. **Log into lpubelts.com**
+
+2. **If you're creating a new LPUbelts account, got to "Edit Profile"  in the menu to set the name that will show up on your Scorecard and the Leaderboard.**
+3. **Select the Scorecard under the user menu in the upper right**
+4. **Click/tap the “Import Dan Sheet” button**
+5. **Type your tab name, exactly as it appears on the [Google Dan Sheet](https://docs.Google.com/spreadsheets/d/1gXQvnFi5usYNcFJKqxxBXYzMczNZ0ml_UVHYrX4D90o/edit?usp=sharing)**
+6. **Click/tap “Import” to confirm**
+
+**That’s it!**
+
+You should now see your dan sheet on lpubelts.com, and can carry on! If you have further interest or questions, then keep on reading, as we go through each step in more detail.
+
+**For new Black Belts without a Dan Sheet:**
+
+- Scorecard entries can be added at any time.
+- You must be already be approved for Black Belt through the normal request process.
+- Create an account & set Display Name on lpubelts.com if you don't have one.
+- Contact the LPUbelts team directly or in #belt-explorer to have your belt approval entered into the system.
+
+
+## DETAILED INSTRUCTIONS
+
+### 1. Log into lpubelts.com.
+
+It’s simple to get started. In the upper right corner you will find the user menu, and then select the option to “Sign In with Google”. You can use any Google account you like for this purpose, and you don’t need to use the same Google account that you have associated with your Google dan sheet.
+
+**If you have never used lpubelts.com before**, go to "Edit Profile" in the same menu to set the Display Name people will see if you share your scorecard. This same name is used to show your spot on the Leaderboard.
+
+![Log into lpubelts.com](https://dev.lpubelts.com/images/sign-in-+-scorecard-link-s.png)
+
+### 2. Select "Scorecard" under the user menu in the upper right.
+
+Once you have logged in, the user menu now has more options. (Current users of pubelts.com will notice that Profile has been replaced by three different things: Lock Collection still has all of your owned, picked, and wishlist locks, Safe Locks allows you to track the safes that you have cracked, and the Scorecard is the new and improved Dan Sheet.) Select Scorecard from the menu.
+
+What you see on your Scorecard depends on whether you have a new account. When you start fresh, you have no locks in your Scorecard. But if you have been dutifully clicking the “Recorded” checkbox on your locks, you will see all of them here on your Scorecard. It may appear that you already have a healthy Scorecard, but the links to all your videos are not yet here. That’s what the unhappy red (!) circle next to each lock is telling you.
+
+![Scorecard entry with no date](https://dev.lpubelts.com/images/no-date-s.png)
+
+By importing your dan sheet, we’re going to address that.
+
+### 3. Click/tap the “Import Dan Sheet” button.
+
+![Import Dan Sheet](https://dev.lpubelts.com/images/import-dan-+-import-form-s.png)
+
+### 4. Type your tab name, exactly as it appears on the Google dan sheet.
+
+   You need to match the tab name exactly as it appears on your dan sheet, paying attention to capital and lowercase letters.
+
+### 5. Click/tap “Import” to confirm.
+
+   The moment of truth! Your scorecard will refresh, now with a bunch of additions! You will see your Dan points and number of black belt locks picked. Below that you’ll see the date that your Black Belt was awarded. All your locks follow, many of them with links, dates, and notes. The links come from your dan sheet and, for public YouTube videos, the date the video was published.
+
+But wait you say, this may not look exactly like your previous dan sheet! Let’s talk through some of the potential differences.
+
+## FREQUENTLY ASKED QUESTIONS
+
+**Q: Why are my points and black belt count different?**
+
+A: For many people the calculations will match exactly. But in general the numbers may not match for several reasons, all of which you can find by clicking the (?) icon in the upper right, just next to your dan point total.
+
+![Exceptions on Import Dan Sheet](https://dev.lpubelts.com/images/exceptions-s.png)
+
+**Could not match.** While the importer does a really good job overall, the lock classification that the dan sheets use is slightly different from the one in lpubelts.com. You may have a lock from your dan sheet that is too ambiguous to match clearly to the right entry in lpubelts.com. No worries, you just need to find that lock in lpubelts.com and add your video link and date manually. Other times the lock may not yet exist in lpubelts.com, because it has not yet been classified. Though this will never be the cause of a point difference, since such locks weren’t worth points there either.
+
+**Missing or bad URLs.** If you had a bunch of Recorded locks before, there is a good chance you still have some of these, as the dan sheet does not usually contain green and below locks. Those aren’t responsible for lost dan points, but not providing a proper link in your dan sheet for a Blue lock or above will certainly do it. All cases are very easy to fix: just click on the lock to edit and add/fix the missing/bad link.
+
+**Upgrades.** This is probably the biggest source of legitimately different points. We have a lock upgrade system, where locks with fewer elements, more mastered pins and such are upgraded by locks with more elements, etc. To date, it hasn’t been very clear which locks upgrade others, and the burden has been on black belt pickers to figure it out and use the “upgraded” modifier in their Google sheet so that only the points for the upgraded lock count. This has been applied very inconsistently across dan sheets, but no more! We now have a single upgrade taxonomy (click on “Upgrades list” towards the top of your scorecard, just below your dan point total, to see it) that is automatically applied consistently to everyone. Since this is the first time anyone has attempted to pull together such a taxonomy, I’m sure some people would judge a specific upgrade pair differently than others. If you disagree with a particular upgrade pair, then please raise the issue with #lock-classification for further review.
+
+**Duplicates.** Some locks are samelined into the same entry, and therefore only one will count for dan points. The Google dan sheet also did an accurate job of calculating this, so this should not result in different points. But it is called out here, to help explain how the math works.
+
+**Q: The link/date/modifier/notes are wrong. Can I fix it?**
+
+A: Yes, please do! The importer was designed to make this transition simpler. If something doesn’t look right, this is your Scorecard, and you should be able to edit the entries that are incorrect. I am not aware of any locks that were incorrectly matched, but if you do run into that, then you can always delete the incorrect entry and add a new one on the correct lock.
+
+**Q: How do I add new entries to my Scorecard?**
+
+A: For new locks, find the lock on the lpubelts.com site, and click the “My Collection” button. There is a new checkbox for “Scorecard”. By checking this box, a form will appear where you can input all the information. Once you save this form, a new entry for this lock will appear on your Scorecard.
+
+For new projects, go to the top of your Scorecard and click on the “Add Project” button. Similar to adding a new lock, a form will appear where you can input all the information. Once you save this form, your project will appear on your Scorecard.
+
+![Scorecard - Add Lock & Add Project](https://dev.lpubelts.com/images/collection-+-add-project-s.png)
+
+**Q: I’ve picked enough locks to get to the next dan while you were talking. How do I get my belt?**
+
+A: First, add all of your locks and projects to your Scorecard, so that it is up-to-date. We are still using **#belt-requests** on discord, and for the moment there is no way within lpubelts.com to request your belt. However, when you are ready for your next dan, then you can copy a link to your scorecard by clicking the link icon just to the right of the “Scorecard” title at the top. Then paste this link into #belt-requests, so your post should look something like this:
+
+\`@LPUBeltBot request 8th Dan https://lpubelts.com/#/profile/[YourID]/scorecard\`
+
+The mods will receive your request, reviewing your Scorecard similar to how they previously reviewed your Google dan sheet. And they will respond just as before, in #belt-requests.
+
+**Q: You still haven’t answered my question!**
+
+A: There are many more additions in this big update, but please drop a note in **#belt-explorer** and the good folks on the Belt Explorer team will help you out! And of course, if you find something broken, please speak up in #belt-explorer so we can fix it.
+`;function k(){const o="8/15/2024",{user:t}=r.useContext(s),n=t?t.uid:"(your id)",a=y.replace("[YourID]",n);return e.jsx(i.Fragment,{children:e.jsxs(l,{style:{maxWidth:760,marginLeft:"auto",marginRight:"auto",marginTop:16,marginButtom:16},children:[e.jsx(h,{children:e.jsx(d,{rehypePlugins:[[p,{target:"_blank"}]],remarkPlugins:[u],children:a})}),e.jsxs(c,{children:["Updated: ",o]})]})})}export{k as default};
---
+++ assets/LeaderboardRoute-C7YKf65M.js
@@ -0,0 +1,1 @@
+import{j as e,R as a,aa as r,h as t,T as s}from"./index-CR79cG60.js";import{u as o}from"./usePageTitle-dtHUpQpn.js";function x(){return o("Leaderboard"),e.jsxs(a.Fragment,{children:[e.jsx(r,{}),e.jsx(t,{}),e.jsx(s,{feature:"leaderboard"})]})}export{x as default};
