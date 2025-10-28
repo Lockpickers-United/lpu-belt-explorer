ed79d096b1038dcccf327db8eb7271c5a78b4cc0
mgsecure <mgsecure@users.noreply.github.com>
2025-10-28 18:06:07 +0000

    deploy: 64c4c85d54a92445b28ff846ccf9f63caf608527

--- assets/usePageTitle-dtHUpQpn.js
+++
@@ -1,1 +0,0 @@
-import{az as o}from"./index-CR79cG60.js";function i(t){return o(n(t))}function n(t){const e=/^localhost.*/.test(window.location.host)?"LOCAL":/^dev.*/.test(window.location.host)?"DEV":"LPU Belt Explorer";return t?`${e} - ${t}`:"LPU Belt Explorer"}export{i as u};
