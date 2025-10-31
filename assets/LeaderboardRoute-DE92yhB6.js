2bcf8b9bf4e73b1b618429b2930976b02fc67dba
mgsecure <mgsecure@users.noreply.github.com>
2025-10-31 17:06:09 +0000

    deploy: 0e51cc3b8ea0df8f92a3123ffbc7992947185cb0

--- assets/LeaderboardRoute-DE92yhB6.js
+++
@@ -1,1 +0,0 @@
-import{j as e,R as a,aa as r,h as t,T as s}from"./index-Cgfn-mCc.js";import{u as o}from"./usePageTitle-HXFpBaDE.js";function x(){return o("Leaderboard"),e.jsxs(a.Fragment,{children:[e.jsx(r,{}),e.jsx(t,{}),e.jsx(s,{feature:"leaderboard"})]})}export{x as default};
