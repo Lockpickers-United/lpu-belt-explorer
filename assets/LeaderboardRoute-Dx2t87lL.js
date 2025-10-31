cabe6b6c85d3d92fe2a92ce52fa3b7071df39cbf
mgsecure <mgsecure@users.noreply.github.com>
2025-10-31 16:02:45 +0000

    deploy: 66466defe12f1040ea84334447fa33e833f9c7ea

--- assets/LeaderboardRoute-Dx2t87lL.js
+++
@@ -1,1 +0,0 @@
-import{j as e,R as a,aa as r,h as t,T as s}from"./index-dKRtSNxw.js";import{u as o}from"./usePageTitle-BuZvY77b.js";function x(){return o("Leaderboard"),e.jsxs(a.Fragment,{children:[e.jsx(r,{}),e.jsx(t,{}),e.jsx(s,{feature:"leaderboard"})]})}export{x as default};
--- assets/usePageTitle-BuZvY77b.js
+++
@@ -1,1 +0,0 @@
-import{az as o}from"./index-dKRtSNxw.js";function i(t){return o(n(t))}function n(t){const e=/^localhost.*/.test(window.location.host)?"LOCAL":/^dev.*/.test(window.location.host)?"DEV":"LPU Belt Explorer";return t?`${e} - ${t}`:"LPU Belt Explorer"}export{i as u};
