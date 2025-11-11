1f21195b5e17cbf2786587f3c3d667be00779e21
mgsecure <mgsecure@users.noreply.github.com>
2025-11-11 18:47:01 +0000

    deploy: b3c586f4f5c0861a8e68ee1ad53853b1c78b1c91

--- assets/ProfileNotFound-BeuXPspM.js
+++
@@ -1,1 +0,0 @@
-import{j as a,ab as t,ar as o,ac as n,Q as i}from"./index-CcyMCo4v.js";function s(){const r={marginTop:16,maxWidth:350,marginLeft:"auto",marginRight:"auto",borderRadius:0};return a.jsxs(t,{style:r,children:[a.jsx(o,{title:"Profile Not Found!"}),a.jsx(n,{children:a.jsx(i,{variant:"h6",align:"center",children:"This profile was not found."})})]})}export{s as P};
