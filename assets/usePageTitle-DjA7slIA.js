59785c1ff7c8912bdc32feeefbf1032ee252a733
mgsecure <mgsecure@users.noreply.github.com>
2025-11-15 19:16:51 +0000

    deploy: 42abd29168714b602e250b1f5e2114fcc0d6c4de

---
+++ assets/usePageTitle-DjA7slIA.js
@@ -0,0 +1,1 @@
+import{ay as o}from"./index-CVYsYHov.js";function i(t){return o(n(t))}function n(t){const e=/^localhost.*/.test(window.location.host)?"LOCAL":/^dev.*/.test(window.location.host)?"DEV":"LPU Belt Explorer";return t?`${e} - ${t}`:"LPU Belt Explorer"}export{i as u};
