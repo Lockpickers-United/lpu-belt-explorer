838b132a7094e586795dd3d1ba315a2dc06375e2
mgsecure <mgsecure@users.noreply.github.com>
2025-12-28 18:10:09 +0000

    deploy: edc251265af8c169b67fe85cbb6e14a4b72c66d3

---
+++ assets/LoadingDisplay-DvbdCGoc.js
@@ -0,0 +1,1 @@
+import{j as a,R as r}from"./index-DBzGRKOZ.js";import{l as o}from"./LPU-DltM2OBF.js";import{L as t}from"./LinearProgress-aQsv9od7.js";function e(){return a.jsxs(r.Fragment,{children:[a.jsx(t,{variant:"indeterminate",color:"secondary"}),a.jsx("img",{alt:"Loading",src:o,style:{marginLeft:"auto",marginRight:"auto",display:"block"}})]})}export{e as L};
