f1d5cfeb8772a59a26c6535325876c2d11ea3e9a
mgsecure <mgsecure@users.noreply.github.com>
2026-02-12 09:07:37 +0000

    deploy: 097ed3c5adbf16c1e7814a1d303b4f710b9b566c

--- assets/CopyEntryTextButton-JuTM-o_d.js
+++
@@ -1,2 +0,0 @@
-import{r as s,Y as i,j as e,X as c,V as l}from"./index-Dr0OGwon.js";import{C as p}from"./ContentCopy-CTsghCHz.js";function d({entry:a}){const r=s.useCallback(async()=>{const n=a.makeModels.map(({make:t,model:o})=>t&&t!==o?`${t} ${o}`:o).join(`
-`);await navigator.clipboard.writeText(n),i("Make/Model text copied to clipboard.")},[a.makeModels]);return e.jsx(c,{title:"Copy Make/Model Text",arrow:!0,disableFocusListener:!0,children:e.jsx(l,{onClick:r,children:e.jsx(p,{})})})}export{d as C};
---
+++ assets/usePageTitle-CL6b8399.js
@@ -0,0 +1,1 @@
+import{aA as o}from"./index-DUr27NUl.js";function i(t){return o(n(t))}function n(t){const e=/^localhost.*/.test(window.location.host)?"LOCAL":/^dev.*/.test(window.location.host)?"DEV":"LPU Belt Explorer";return t?`${e} - ${t}`:"LPU Belt Explorer"}export{i as u};
