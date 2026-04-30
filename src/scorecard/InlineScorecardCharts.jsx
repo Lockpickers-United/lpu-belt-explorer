b9f60df1043fe3dd8540807413b7e8f6a8371b7b
mgsecure <125900659+mgsecure@users.noreply.github.com>
2026-04-30 16:17:05 -0700

    remove <ScorecardBeltBar beltData={chartData}/> to fix scorecard

--- src/scorecard/InlineScorecardCharts.jsx
+++ src/scorecard/InlineScorecardCharts.jsx
@@ -39,7 +39,6 @@ function InlineCollectionCharts({entries}) {
 
     return (
         <div style={{width: '100%', marginTop: '0px'}}>
-            <ScorecardBeltBar beltData={chartData}/>
         </div>
     )
 }
