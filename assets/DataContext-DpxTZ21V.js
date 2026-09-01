a7350b5cefd9510d50b40d8eb1766ec10cc335d7
mgsecure <mgsecure@users.noreply.github.com>
2026-09-01 11:59:06 +0000

    deploy: 8135ce2b71a42864e17181fa3715cfa555b0530a

--- assets/DataContext-DpxTZ21V.js
+++
@@ -1,1 +0,0 @@
-import{R as t}from"./index-BJzWWu4P.js";const e=t.createContext({});export{e as D};
---
+++ assets/ScorecardInfoRoute-D-SbYqyh.js
@@ -0,0 +1,1 @@
+import{j as r,R as e,N as a,a9 as o,h as s,T as t}from"./index-QK5ZN6jG.js";import{u as c}from"./usePageTitle-r4BI2Xzc.js";function u(){return c("Scorecard Info"),r.jsxs(e.Fragment,{children:[r.jsx(a,{title:"Scorecard Info"}),r.jsx(o,{}),r.jsx(s,{}),r.jsx(t,{feature:"scorecard-info"})]})}export{u as default};
