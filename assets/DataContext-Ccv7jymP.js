3abe00c0610db0b28d9b4372170ebc39e936b7af
mgsecure <mgsecure@users.noreply.github.com>
2026-09-12 20:29:59 +0000

    deploy: 5f3d646a8abb78202d36760f4ac91267a8f13c3e

---
+++ assets/DataContext-Ccv7jymP.js
@@ -0,0 +1,1 @@
+import{R as t}from"./index-DTNITZ9G.js";const e=t.createContext({});export{e as D};
---
+++ assets/ProfileParentRoute-Ck2vyZ9o.js
@@ -0,0 +1,1 @@
+import{M as s,j as r,ab as t}from"./index-DTNITZ9G.js";import{P as o}from"./ProfileContext-Ci0h-Go_.js";function u(){const{userId:e}=s();return r.jsx(o,{userId:e,children:r.jsx(t,{})})}export{u as default};
