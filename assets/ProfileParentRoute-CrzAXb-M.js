699a57c6e80d82ad88fa018172a02a2988f8dd47
mgsecure <mgsecure@users.noreply.github.com>
2026-09-12 00:50:06 +0000

    deploy: ccd8884873958f594f232ea0be4213f03ba2d2ce

---
+++ assets/ProfileParentRoute-CrzAXb-M.js
@@ -0,0 +1,1 @@
+import{M as s,j as r,ab as t}from"./index-GFAqMUOR.js";import{P as o}from"./ProfileContext-CxRC2O3n.js";function u(){const{userId:e}=s();return r.jsx(o,{userId:e,children:r.jsx(t,{})})}export{u as default};
---
+++ assets/RankingRequestsParentRoute-D9F0Ksnn.js
@@ -0,0 +1,1 @@
+import{j as t,R as e,ab as s,k as a}from"./index-GFAqMUOR.js";function n(){return t.jsxs(e.Fragment,{children:[t.jsx(s,{}),t.jsx(a,{})]})}export{n as default};
