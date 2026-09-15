c1a33951201c472af70a35df44d3406b9bacd8c8
mgsecure <mgsecure@users.noreply.github.com>
2026-09-15 15:40:07 +0000

    deploy: 3282cfbdc0f9e5b4d0890b5bdcc78dbef853c1ed

--- assets/ProfileParentRoute-BAxwmoja.js
+++
@@ -1,1 +0,0 @@
-import{M as s,j as r,ab as t}from"./index-DEdxcdSA.js";import{P as o}from"./ProfileContext-DUlEKEcG.js";function u(){const{userId:e}=s();return r.jsx(o,{userId:e,children:r.jsx(t,{})})}export{u as default};
